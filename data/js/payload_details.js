dd82a31f4b43142d265690e68f6d23f483a43a9b
ellenforsyth <ellenforsyth@gmail.com>
2018-11-04 18:15:02 +1100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 34872,
-	"created_at": "Sat Nov 03 07:15:03 +0000 2018",
+	"tweets": 34875,
+	"created_at": "Sun Nov 04 07:15:01 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,522 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 03 22:00:01 +0000 2018",
+		"id": 1058841251007479800,
+		"id_str": "1058841251007479808",
+		"full_text": "The National Library collects a copy of every book published in Australia, this is called Legal Deposit. Every day we receive upward of 60-80 new books. \n\nOur very adaptable cataloguers are ready to catalogue anything from Australian law to Goosebumps on any given day. #shelfie https://t.co/sO3h11fmGX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "shelfie",
+					"indices": [
+						270,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1058213903316922400,
+					"id_str": "1058213903316922369",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq-H8DuVsAE9BFk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq-H8DuVsAE9BFk.jpg",
+					"url": "https://t.co/sO3h11fmGX",
+					"display_url": "pic.twitter.com/sO3h11fmGX",
+					"expanded_url": "https://twitter.com/nlagovau/status/1058841251007479808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058213903316922400,
+					"id_str": "1058213903316922369",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq-H8DuVsAE9BFk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq-H8DuVsAE9BFk.jpg",
+					"url": "https://t.co/sO3h11fmGX",
+					"display_url": "pic.twitter.com/sO3h11fmGX",
+					"expanded_url": "https://twitter.com/nlagovau/status/1058841251007479808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 42354375,
+			"id_str": "42354375",
+			"name": "National Library Aus",
+			"screen_name": "nlagovau",
+			"location": "Canberra, Australia",
+			"description": "We collect, preserve and make accessible the best Australian stories. Read all about it.",
+			"url": "http://t.co/wb67iCGQsu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/wb67iCGQsu",
+							"expanded_url": "http://www.nla.gov.au",
+							"display_url": "nla.gov.au",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 41452,
+			"friends_count": 220,
+			"listed_count": 995,
+			"created_at": "Mon May 25 05:09:03 +0000 2009",
+			"favourites_count": 7349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11051,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1001285927136718848/0TmXDd3r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1001285927136718848/0TmXDd3r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/42354375/1537744316",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26,
+		"favorite_count": 104,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The National Library collects a copy of every book published in Australia, this is called Legal Deposit. Every day we receive upward of 60-80 new books. \n\nOur very adaptable cataloguers are ready to catalogue anything from Australian law to Goosebumps on any given day. #shelfie https://t.co/sO3h11fmGX"
+	},
+	{
+		"created_at": "Sun Nov 04 03:16:29 +0000 2018",
+		"id": 1058920894339240000,
+		"id_str": "1058920894339239937",
+		"full_text": "@cfwriter They seem slightly threatening 🐈",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cfwriter",
+					"name": "Dr Rachel Franks",
+					"id": 52325959,
+					"id_str": "52325959",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1058890606599012400,
+		"in_reply_to_status_id_str": "1058890606599012352",
+		"in_reply_to_user_id": 52325959,
+		"in_reply_to_user_id_str": "52325959",
+		"in_reply_to_screen_name": "cfwriter",
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Currently in Eora country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2465,
+			"friends_count": 746,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 4312,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 34763,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "B80707",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@cfwriter They seem slightly threatening 🐈"
+	},
+	{
+		"created_at": "Sat Nov 03 10:00:44 +0000 2018",
+		"id": 1058660240592388100,
+		"id_str": "1058660240592388096",
+		"full_text": "From Beowulf to bramble thickets to British kings: how one charter, written in 931, gives us clues to the Anglo-Saxon landscape #BLAngloSaxons https://t.co/LH2M2fvoG6 https://t.co/Blt1uwyyzS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLAngloSaxons",
+					"indices": [
+						128,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LH2M2fvoG6",
+					"expanded_url": "https://blogs.bl.uk/digitisedmanuscripts/2018/11/the-real-lake-of-grendel.html",
+					"display_url": "blogs.bl.uk/digitisedmanus…",
+					"indices": [
+						143,
+						166
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1058660229154406400,
+					"id_str": "1058660229154406400",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/DrEd3pCVAAAc4Jb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrEd3pCVAAAc4Jb.jpg",
+					"url": "https://t.co/Blt1uwyyzS",
+					"display_url": "pic.twitter.com/Blt1uwyyzS",
+					"expanded_url": "https://twitter.com/BLMedieval/status/1058660240592388096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 479,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058660229154406400,
+					"id_str": "1058660229154406400",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/DrEd3pCVAAAc4Jb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrEd3pCVAAAc4Jb.jpg",
+					"url": "https://t.co/Blt1uwyyzS",
+					"display_url": "pic.twitter.com/Blt1uwyyzS",
+					"expanded_url": "https://twitter.com/BLMedieval/status/1058660240592388096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 479,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1058660234997035000,
+					"id_str": "1058660234997035008",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/DrEd3-zUcAApP2v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrEd3-zUcAApP2v.jpg",
+					"url": "https://t.co/Blt1uwyyzS",
+					"display_url": "pic.twitter.com/Blt1uwyyzS",
+					"expanded_url": "https://twitter.com/BLMedieval/status/1058660240592388096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 425,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://twittimer.com\" rel=\"nofollow\">Twittimer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 468249929,
+			"id_str": "468249929",
+			"name": "Medieval Manuscripts",
+			"screen_name": "BLMedieval",
+			"location": "British Library, London",
+			"description": "Tweeting about the British Library's marvellous medieval manuscripts",
+			"url": "https://t.co/oAxMoR30QJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oAxMoR30QJ",
+							"expanded_url": "http://blogs.bl.uk/digitisedmanuscripts/",
+							"display_url": "blogs.bl.uk/digitisedmanus…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76986,
+			"friends_count": 199,
+			"listed_count": 1993,
+			"created_at": "Thu Jan 19 10:18:17 +0000 2012",
+			"favourites_count": 3630,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17521,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1766064061/E070014_for_Twitter_normal.JPG",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1766064061/E070014_for_Twitter_normal.JPG",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/468249929/1398941850",
+			"profile_link_color": "CF002A",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 98,
+		"favorite_count": 262,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "From Beowulf to bramble thickets to British kings: how one charter, written in 931, gives us clues to the Anglo-Saxon landscape #BLAngloSaxons https://t.co/LH2M2fvoG6 https://t.co/Blt1uwyyzS"
+	},
+	{
 		"created_at": "Fri Nov 02 20:02:02 +0000 2018",
 		"id": 1058449171965915100,
 		"id_str": "1058449171965915136",
