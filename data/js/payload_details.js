f4ab573b8be584e99c74d4c8dfe9335dd60ff9ea
ellenforsyth <ellenforsyth@gmail.com>
2018-12-04 18:15:02 +1100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 35146,
-	"created_at": "Sun Dec 02 07:15:03 +0000 2018",
+	"tweets": 35148,
+	"created_at": "Tue Dec 04 07:15:00 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,474 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Mon Dec 03 08:00:25 +0000 2018",
+		"id": 1069501597896646700,
+		"id_str": "1069501597896646656",
+		"full_text": "The #AussieBirdCount results are in! Together we counted over 2.7m birds including 305k Rainbow Lorikeets! This puts the Rainbow Lorikeet at the top of the count for 5 consecutive years. Why are Rainbows ruling the roost? Head to the website to find out: https://t.co/ie8hHuyQdM https://t.co/RZQFkAP9gO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AussieBirdCount",
+					"indices": [
+						4,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ie8hHuyQdM",
+					"expanded_url": "http://bit.ly/2018ABBCResults",
+					"display_url": "bit.ly/2018ABBCResults",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1069501595359092700,
+					"id_str": "1069501595359092737",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/DteiCNYXoAE1jZM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DteiCNYXoAE1jZM.jpg",
+					"url": "https://t.co/RZQFkAP9gO",
+					"display_url": "pic.twitter.com/RZQFkAP9gO",
+					"expanded_url": "https://twitter.com/BirdlifeOz/status/1069501597896646656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1484,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 493,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 869,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1069501595359092700,
+					"id_str": "1069501595359092737",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/DteiCNYXoAE1jZM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DteiCNYXoAE1jZM.jpg",
+					"url": "https://t.co/RZQFkAP9gO",
+					"display_url": "pic.twitter.com/RZQFkAP9gO",
+					"expanded_url": "https://twitter.com/BirdlifeOz/status/1069501597896646656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1484,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 493,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 869,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1069501595841384400,
+					"id_str": "1069501595841384448",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/DteiCPLW0AALxbD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DteiCPLW0AALxbD.jpg",
+					"url": "https://t.co/RZQFkAP9gO",
+					"display_url": "pic.twitter.com/RZQFkAP9gO",
+					"expanded_url": "https://twitter.com/BirdlifeOz/status/1069501597896646656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1590,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 931,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 528,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1069501595396776000,
+					"id_str": "1069501595396775937",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/DteiCNhWoAEG_xU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DteiCNhWoAEG_xU.jpg",
+					"url": "https://t.co/RZQFkAP9gO",
+					"display_url": "pic.twitter.com/RZQFkAP9gO",
+					"expanded_url": "https://twitter.com/BirdlifeOz/status/1069501597896646656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1772,
+							"h": 1734,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 665,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 145136771,
+			"id_str": "145136771",
+			"name": "BirdLife Australia",
+			"screen_name": "BirdlifeOz",
+			"location": "Australia",
+			"description": "BirdLife Australia is dedicated to creating a bright future for Australia's birds",
+			"url": "http://t.co/1FnHo8lZVd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1FnHo8lZVd",
+							"expanded_url": "http://birdlife.org.au",
+							"display_url": "birdlife.org.au",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13572,
+			"friends_count": 822,
+			"listed_count": 312,
+			"created_at": "Tue May 18 06:05:23 +0000 2010",
+			"favourites_count": 9297,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7655,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1580EB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1784228777/twitter-logo-bird_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1784228777/twitter-logo-bird_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/145136771/1543875510",
+			"profile_link_color": "003A80",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "B6B8B1",
+			"profile_text_color": "0F0E0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 42,
+		"favorite_count": 72,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The #AussieBirdCount results are in! Together we counted over 2.7m birds including 305k Rainbow Lorikeets! This puts the Rainbow Lorikeet at the top of the count for 5 consecutive years. Why are Rainbows ruling the roost? Head to the website to find out: https://t.co/ie8hHuyQdM https://t.co/RZQFkAP9gO"
+	},
+	{
+		"created_at": "Mon Dec 03 09:40:22 +0000 2018",
+		"id": 1069526751011688400,
+		"id_str": "1069526751011688448",
+		"full_text": "Nice https://t.co/fdO3MJtKOz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fdO3MJtKOz",
+					"expanded_url": "https://twitter.com/karenbeath/status/1069350603132817409",
+					"display_url": "twitter.com/karenbeath/sta…",
+					"indices": [
+						5,
+						28
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Currently in Eora country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2462,
+			"friends_count": 751,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 4407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35011,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "B80707",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1069350603132817400,
+		"quoted_status_id_str": "1069350603132817409",
+		"quoted_status_permalink": {
+			"url": "https://t.co/fdO3MJtKOz",
+			"expanded": "https://twitter.com/karenbeath/status/1069350603132817409",
+			"display": "twitter.com/karenbeath/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Dec 02 22:00:26 +0000 2018",
+			"id": 1069350603132817400,
+			"id_str": "1069350603132817409",
+			"full_text": "Pick up a book and pedal as more Montreal libraries get bicycle desks\nhttps://t.co/xLY9VoA5aq",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				93
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/xLY9VoA5aq",
+						"expanded_url": "https://www.cbc.ca/news/canada/montreal/libraries-montreal-bicycle-desks-1.4901689",
+						"display_url": "cbc.ca/news/canada/mo…",
+						"indices": [
+							70,
+							93
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 299662198,
+				"id_str": "299662198",
+				"name": "Karen Beath",
+				"screen_name": "KarenBeath",
+				"location": "Brisbane, Queensland",
+				"description": "Librarian, trail runner, hiking enthusiast, traveller, giant nerd. Views are my own.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 773,
+				"friends_count": 1273,
+				"listed_count": 62,
+				"created_at": "Mon May 16 13:49:23 +0000 2011",
+				"favourites_count": 1284,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3877,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DBE9ED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000744868580/823e3196e588f99bfa15aa07947e6e43_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000744868580/823e3196e588f99bfa15aa07947e6e43_normal.jpeg",
+				"profile_link_color": "CC3366",
+				"profile_sidebar_border_color": "DBE9ED",
+				"profile_sidebar_fill_color": "E6F6F9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Nice https://t.co/fdO3MJtKOz"
+	},
+	{
 		"created_at": "Sat Dec 01 09:47:26 +0000 2018",
 		"id": 1068803753673781200,
 		"id_str": "1068803753673781248",
