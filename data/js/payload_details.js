3d9ddbd4669faed665d2453abb815c1cc3e9fd6e
ellenforsyth <ellenforsyth@gmail.com>
2019-01-13 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 35727,
-	"created_at": "Sat Jan 12 07:15:01 +0000 2019",
+	"tweets": 35728,
+	"created_at": "Sun Jan 13 07:15:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 12 19:48:17 +0000 2019",
+		"id": 1084175250223317000,
+		"id_str": "1084175250223316992",
+		"full_text": "an impressive account https://t.co/rooxZehBwl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rooxZehBwl",
+					"expanded_url": "https://twitter.com/OUPAcademic/status/1084066231718539264",
+					"display_url": "twitter.com/OUPAcademic/st…",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Currently in Eora country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2468,
+			"friends_count": 759,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 4547,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35132,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1556570112/IMG_0101_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "B80707",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1084066231718539300,
+		"quoted_status_id_str": "1084066231718539264",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rooxZehBwl",
+			"expanded": "https://twitter.com/OUPAcademic/status/1084066231718539264",
+			"display": "twitter.com/OUPAcademic/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jan 12 12:35:05 +0000 2019",
+			"id": 1084066231718539300,
+			"id_str": "1084066231718539264",
+			"full_text": "Learn how the Global Alliance to Immunize Against AIDS Vaccine Foundation used the arts within their community to address local needs. https://t.co/LmEruGhUhj",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				158
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/LmEruGhUhj",
+						"expanded_url": "https://oxford.ly/2SLEHCm",
+						"display_url": "oxford.ly/2SLEHCm",
+						"indices": [
+							135,
+							158
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.percolate.com\" rel=\"nofollow\">Percolate</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 6580122,
+				"id_str": "6580122",
+				"name": "Oxford Academic",
+				"screen_name": "OUPAcademic",
+				"location": "New York City & Oxford",
+				"description": "Oxford University Press’s academic news and insights for the thinking world, brought to you by OUP's social media team.",
+				"url": "http://t.co/JU3e2aMphQ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/JU3e2aMphQ",
+								"expanded_url": "http://blog.oup.com",
+								"display_url": "blog.oup.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 89397,
+				"friends_count": 3409,
+				"listed_count": 2600,
+				"created_at": "Mon Jun 04 21:33:13 +0000 2007",
+				"favourites_count": 3275,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 53368,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "BFBFBF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/583516526742999040/HWzESF4N_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/583516526742999040/HWzESF4N_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/6580122/1398257568",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "002147",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "an impressive account https://t.co/rooxZehBwl"
+	},
+	{
 		"created_at": "Tue Jan 08 13:00:36 +0000 2019",
 		"id": 1082623102830501900,
 		"id_str": "1083893028673093632",
