1496b3c0fa86d269e1e559c8ea2acd2d070bd088
ellenforsyth <ellenforsyth@gmail.com>
2019-05-02 17:15:02 +1000

    Added 1 tweets

---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,182 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 20:22:14 +0000 2019",
+		"id": 1123482348937519100,
+		"id_str": "1123684073812176897",
+		"full_text": "Slashing climate funding &amp; programs, rejecting expert advice, misleading claims, no credible climate policy, and covering up poor performance: Our latest report details the defining leadership failure of the last decade. https://t.co/anV5X2sys8  #auspol https://t.co/x3Ah48YZRy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "auspol",
+					"indices": [
+						250,
+						257
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/anV5X2sys8",
+					"expanded_url": "https://climc.nl/CCCC",
+					"display_url": "climc.nl/CCCC",
+					"indices": [
+						225,
+						248
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1123482347305865200,
+					"id_str": "1123482347305865216",
+					"indices": [
+						258,
+						281
+					],
+					"media_url": "http://pbs.twimg.com/media/D5dpPg6WkAAi69p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5dpPg6WkAAi69p.jpg",
+					"url": "https://t.co/x3Ah48YZRy",
+					"display_url": "pic.twitter.com/x3Ah48YZRy",
+					"expanded_url": "https://twitter.com/climatecouncil/status/1123482348937519106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 973,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 551,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1668,
+							"h": 1352,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123482347305865200,
+					"id_str": "1123482347305865216",
+					"indices": [
+						258,
+						281
+					],
+					"media_url": "http://pbs.twimg.com/media/D5dpPg6WkAAi69p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5dpPg6WkAAi69p.jpg",
+					"url": "https://t.co/x3Ah48YZRy",
+					"display_url": "pic.twitter.com/x3Ah48YZRy",
+					"expanded_url": "https://twitter.com/climatecouncil/status/1123482348937519106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 973,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 551,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1668,
+							"h": 1352,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1892683062,
+			"id_str": "1892683062",
+			"name": "Climate Council",
+			"screen_name": "climatecouncil",
+			"location": "Australia",
+			"description": "Providing Australians with a reliable source of information on climate science. \nAuthorised by A. McKenzie, 8 Short Street Surry Hills NSW 2010",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 44906,
+			"friends_count": 17879,
+			"listed_count": 960,
+			"created_at": "Sun Sep 22 06:29:31 +0000 2013",
+			"favourites_count": 1860,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17258,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/504862206479970304/rWWL00qW_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/504862206479970304/rWWL00qW_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1892683062/1516252215",
+			"profile_link_color": "EFAB23",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Slashing climate funding &amp; programs, rejecting expert advice, misleading claims, no credible climate policy, and covering up poor performance: Our latest report details the defining leadership failure of the last decade. https://t.co/anV5X2sys8  #auspol https://t.co/x3Ah48YZRy"
+	}
+]
