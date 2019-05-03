f46a2574e8ef35129c7a85943ce426d158551bef
ellenforsyth <ellenforsyth@gmail.com>
2019-05-03 17:15:02 +1000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36142,
-	"created_at": "Thu May 02 07:15:00 +0000 2019",
+	"tweets": 36145,
+	"created_at": "Fri May 03 07:15:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,682 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 19:52:25 +0000 2019",
+		"id": 1124038954485993500,
+		"id_str": "1124038954485993472",
+		"full_text": "helpful to read this evaluation https://t.co/2KXsl7uOG8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2KXsl7uOG8",
+					"expanded_url": "https://twitter.com/BioDivLibrary/status/1123996098715168768",
+					"display_url": "twitter.com/BioDivLibrary/…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly in Gadigal or Gamilaroi country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2494,
+			"friends_count": 798,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5123,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35603,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "990066",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1123996098715168800,
+		"quoted_status_id_str": "1123996098715168768",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2KXsl7uOG8",
+			"expanded": "https://twitter.com/BioDivLibrary/status/1123996098715168768",
+			"display": "twitter.com/BioDivLibrary/…"
+		},
+		"quoted_status": {
+			"created_at": "Thu May 02 17:02:07 +0000 2019",
+			"id": 1123996098715168800,
+			"id_str": "1123996098715168768",
+			"full_text": "This past #WomensHistoryMonth, we were delighted to join with our partners and the wider community to celebrate #WomenInScience for #HerNaturalHistory. We invite you to explore the campaign outcomes and impact in our report. Learn more ➡️ https://t.co/1Xfg2IXCRk https://t.co/jhZWiKx24X",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				262
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "WomensHistoryMonth",
+						"indices": [
+							10,
+							29
+						]
+					},
+					{
+						"text": "WomenInScience",
+						"indices": [
+							112,
+							127
+						]
+					},
+					{
+						"text": "HerNaturalHistory",
+						"indices": [
+							132,
+							150
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/1Xfg2IXCRk",
+						"expanded_url": "https://s.si.edu/2GtQe4u",
+						"display_url": "s.si.edu/2GtQe4u",
+						"indices": [
+							239,
+							262
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1123996095691018200,
+						"id_str": "1123996095691018242",
+						"indices": [
+							263,
+							286
+						],
+						"media_url": "http://pbs.twimg.com/media/D5k8fmyW0AIO_o2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5k8fmyW0AIO_o2.jpg",
+						"url": "https://t.co/jhZWiKx24X",
+						"display_url": "pic.twitter.com/jhZWiKx24X",
+						"expanded_url": "https://twitter.com/BioDivLibrary/status/1123996098715168768/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 505,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1100,
+								"h": 817,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1100,
+								"h": 817,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1123996095691018200,
+						"id_str": "1123996095691018242",
+						"indices": [
+							263,
+							286
+						],
+						"media_url": "http://pbs.twimg.com/media/D5k8fmyW0AIO_o2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5k8fmyW0AIO_o2.jpg",
+						"url": "https://t.co/jhZWiKx24X",
+						"display_url": "pic.twitter.com/jhZWiKx24X",
+						"expanded_url": "https://twitter.com/BioDivLibrary/status/1123996098715168768/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 505,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1100,
+								"h": 817,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1100,
+								"h": 817,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20813165,
+				"id_str": "20813165",
+				"name": "BHL",
+				"screen_name": "BioDivLibrary",
+				"location": "everywhere, all the time",
+				"description": "Biodiversity Heritage Library provides free & #openaccess to #biodiversity literature online. Legal: https://t.co/dJOZaQ7IFu",
+				"url": "http://t.co/uVc0sELg3X",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/uVc0sELg3X",
+								"expanded_url": "http://www.biodiversitylibrary.org",
+								"display_url": "biodiversitylibrary.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/dJOZaQ7IFu",
+								"expanded_url": "http://s.si.edu/legal",
+								"display_url": "s.si.edu/legal",
+								"indices": [
+									101,
+									124
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 22569,
+				"friends_count": 1418,
+				"listed_count": 1095,
+				"created_at": "Fri Feb 13 22:26:16 +0000 2009",
+				"favourites_count": 30744,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 32882,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1542638971/BHL_Small_Logo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542638971/BHL_Small_Logo_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20813165/1479386651",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "helpful to read this evaluation https://t.co/2KXsl7uOG8"
+	},
+	{
+		"created_at": "Thu May 02 19:48:08 +0000 2019",
+		"id": 1124037876625309700,
+		"id_str": "1124037876625309702",
+		"full_text": "Top of the web - January to March 2019 https://t.co/TQPPPWSE7z via @ChristchurchLib I always enjoy reading these statistics",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ChristchurchLib",
+					"name": "Ngā Kete Wānanga-o-Ōtautahi",
+					"id": 190042686,
+					"id_str": "190042686",
+					"indices": [
+						67,
+						83
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TQPPPWSE7z",
+					"expanded_url": "https://cclbibliofile.wordpress.com/2019/05/01/top-of-the-web-january-to-march-2019/",
+					"display_url": "cclbibliofile.wordpress.com/2019/05/01/top…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly in Gadigal or Gamilaroi country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2494,
+			"friends_count": 798,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5123,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35603,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "990066",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Top of the web - January to March 2019 https://t.co/TQPPPWSE7z via @ChristchurchLib I always enjoy reading these statistics"
+	},
+	{
+		"created_at": "Thu May 02 10:31:38 +0000 2019",
+		"id": 1123694027503800300,
+		"id_str": "1123897832031780865",
+		"full_text": "Thank you to #Australia for over 66,000 records of 176 species in the first year of #FrogID \n\nWe present the first #FrogID scientific paper: \"FrogID: Citizen Scientists Provide Validated\nBiodiversity Data on #Frogs of Australia\" in @Herpconbio #WildOz \n\nhttps://t.co/t0OuA1ZKSC https://t.co/TQ4NDjgOtO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Australia",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"text": "FrogID",
+					"indices": [
+						84,
+						91
+					]
+				},
+				{
+					"text": "FrogID",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"text": "Frogs",
+					"indices": [
+						208,
+						214
+					]
+				},
+				{
+					"text": "WildOz",
+					"indices": [
+						244,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Herpconbio",
+					"name": "Herpconbio",
+					"id": 20106330,
+					"id_str": "20106330",
+					"indices": [
+						232,
+						243
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/t0OuA1ZKSC",
+					"expanded_url": "http://www.herpconbio.org/Volume_14/Issue_1/Rowley_etal_2019.pdf",
+					"display_url": "herpconbio.org/Volume_14/Issu…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1123693541006479400,
+					"id_str": "1123693541006479360",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D5gpUmwXkAAFzXe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5gpUmwXkAAFzXe.jpg",
+					"url": "https://t.co/TQ4NDjgOtO",
+					"display_url": "pic.twitter.com/TQ4NDjgOtO",
+					"expanded_url": "https://twitter.com/jodirowley/status/1123694027503800320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1073,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 608,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1538,
+							"h": 1375,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123693541006479400,
+					"id_str": "1123693541006479360",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/D5gpUmwXkAAFzXe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5gpUmwXkAAFzXe.jpg",
+					"url": "https://t.co/TQ4NDjgOtO",
+					"display_url": "pic.twitter.com/TQ4NDjgOtO",
+					"expanded_url": "https://twitter.com/jodirowley/status/1123694027503800320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1073,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 608,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1538,
+							"h": 1375,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 216979622,
+			"id_str": "216979622",
+			"name": "Jodi Rowley",
+			"screen_name": "jodirowley",
+			"location": "Australian Museum & UNSW",
+			"description": "Conservation #biologist obsessed with #frogs. Chief Scientist, #FrogID. Searching for the Peppered Tree #Frog. @austmus & @UNSW, @natgeo Explorer. Views my own.",
+			"url": "https://t.co/WDoBwYhDYS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WDoBwYhDYS",
+							"expanded_url": "http://www.jodirowley.com",
+							"display_url": "jodirowley.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7548,
+			"friends_count": 1491,
+			"listed_count": 234,
+			"created_at": "Thu Nov 18 07:08:51 +0000 2010",
+			"favourites_count": 15617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9968,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "2E3B39",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/816499907288711169/Au5kRUr3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/816499907288711169/Au5kRUr3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/216979622/1398342662",
+			"profile_link_color": "F0593E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "D4C8D4",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 46,
+		"favorite_count": 116,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you to #Australia for over 66,000 records of 176 species in the first year of #FrogID \n\nWe present the first #FrogID scientific paper: \"FrogID: Citizen Scientists Provide Validated\nBiodiversity Data on #Frogs of Australia\" in @Herpconbio #WildOz \n\nhttps://t.co/t0OuA1ZKSC https://t.co/TQ4NDjgOtO"
+	},
+	{
 		"created_at": "Wed May 01 20:22:14 +0000 2019",
 		"id": 1123482348937519100,
 		"id_str": "1123684073812176897",
