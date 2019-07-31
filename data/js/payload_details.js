adbec11b3621f441f232dbb5a42af8ac0fced41c
ellenforsyth <ellenforsyth@gmail.com>
2019-07-31 17:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36347,
-	"created_at": "Tue Jul 30 07:15:01 +0000 2019",
+	"tweets": 36348,
+	"created_at": "Wed Jul 31 07:15:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 30 20:24:34 +0000 2019",
+		"id": 1156299565793812500,
+		"id_str": "1156299565793812480",
+		"full_text": "How Australians spend their time on the net https://t.co/Xb9oQfW8Va via @Sayraphim interesting data with links for even more",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Sayraphim",
+					"name": "Sir Sayraphim",
+					"id": 267534892,
+					"id_str": "267534892",
+					"indices": [
+						72,
+						82
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Xb9oQfW8Va",
+					"expanded_url": "http://sayraphimlothian.com/how-australians-spend-their-time-on-the-net/",
+					"display_url": "sayraphimlothian.com/how-australian…",
+					"indices": [
+						44,
+						67
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly in Gadigal or Gamilaroi country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2502,
+			"friends_count": 796,
+			"listed_count": 231,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5307,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How Australians spend their time on the net https://t.co/Xb9oQfW8Va via @Sayraphim interesting data with links for even more"
+	},
+	{
 		"created_at": "Mon Jul 29 20:00:17 +0000 2019",
 		"id": 1155022209406009300,
 		"id_str": "1155931068505055235",
@@ -227,7 +344,7 @@ Grailbird.data.tweets_2019_07 =
 			"name": "˗ˏˋ Doug Belshaw ˎˊ˗ 🇪🇺 ☠️ ✊",
 			"screen_name": "dajbelshaw",
 			"location": "Morpeth, England",
-			"description": "Open Educational Thinkerer. Husband of @hbelshaw and father of two. Now: @Moodle and @WeAreOpenCoop. Then: @Mozilla / @Jisc / SLT / teacher. Likes: 🏔📚🥃⚽🏊‍♂️",
+			"description": "Open Educational Thinkerer. Husband of @hbelshaw and father of two. Now: @Moodle and @WeAreOpenCoop. Then: @Mozilla / @Jisc / SLT / teacher. Likes: 🏔📚🥃⚽🏊♂️",
 			"url": "https://t.co/pFjCnpa480",
 			"entities": {
 				"url": {
