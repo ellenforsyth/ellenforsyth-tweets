7ce12b289af79b92ae34fb26ba807bbc4a6ab461
ellenforsyth <ellenforsyth@gmail.com>
2019-08-28 17:15:03 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36395,
-	"created_at": "Tue Aug 27 07:15:01 +0000 2019",
+	"tweets": 36396,
+	"created_at": "Wed Aug 28 07:15:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,145 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 27 22:56:13 +0000 2019",
+		"id": 1166341193569046500,
+		"id_str": "1166484591151960064",
+		"full_text": "Congratulations Helsinki Central Library @oodihelsinki on being chosen The Best New Public Library in the World! ✨🏆🎉 #helsinki #visithelsinki #finland\nhttps://t.co/jOXbXyfHaW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			174
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "helsinki",
+					"indices": [
+						117,
+						126
+					]
+				},
+				{
+					"text": "visithelsinki",
+					"indices": [
+						127,
+						141
+					]
+				},
+				{
+					"text": "finland",
+					"indices": [
+						142,
+						150
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "oodihelsinki",
+					"name": "Keskustakirjasto Oodi",
+					"id": 994899114235256800,
+					"id_str": "994899114235256832",
+					"indices": [
+						41,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jOXbXyfHaW",
+					"expanded_url": "https://www.oodihelsinki.fi/en/helsinki-central-library-oodi-chosen-as-the-best-new-public-library-in-the-world/?fbclid=IwAR2LBjcwoI7HnfghjHAbjJmljM32yGsph4wLpMY7kobEd6yKv03hhmaSNLU",
+					"display_url": "oodihelsinki.fi/en/helsinki-ce…",
+					"indices": [
+						151,
+						174
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 92454221,
+			"id_str": "92454221",
+			"name": "Visit Helsinki",
+			"screen_name": "VisitHelsinki",
+			"location": "Helsinki, Finland",
+			"description": "Official Twitter account for Visit Helsinki. Tourist information, articles, curiosities, news, tips, events and photos of the capital of Finland. #Helsinki",
+			"url": "https://t.co/bOvn75pxvE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bOvn75pxvE",
+							"expanded_url": "http://www.myhelsinki.fi",
+							"display_url": "myhelsinki.fi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 51012,
+			"friends_count": 270,
+			"listed_count": 669,
+			"created_at": "Wed Nov 25 05:24:54 +0000 2009",
+			"favourites_count": 11249,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8105,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DAE9EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1013706956916346880/_rVl3lZ4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1013706956916346880/_rVl3lZ4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/92454221/1555323773",
+			"profile_link_color": "82BDE1",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 195,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations Helsinki Central Library @oodihelsinki on being chosen The Best New Public Library in the World! ✨🏆🎉 #helsinki #visithelsinki #finland\nhttps://t.co/jOXbXyfHaW"
+	},
+	{
 		"created_at": "Tue Aug 27 04:52:12 +0000 2019",
 		"id": 1166211788498751500,
 		"id_str": "1166211788498751488",
