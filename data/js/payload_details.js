637f47bf6ffa4ccacf4ce8c15364aac6707194da
ellenforsyth <ellenforsyth@gmail.com>
2019-09-04 17:15:02 +1000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36404,
-	"created_at": "Tue Sep 03 07:15:00 +0000 2019",
+	"tweets": 36406,
+	"created_at": "Wed Sep 04 07:15:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,540 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 03 22:28:34 +0000 2019",
+		"id": 1168992250233274400,
+		"id_str": "1169014346686652416",
+		"full_text": "It's Indigenous Literacy Day! A time to celebrate #IndigenousCulture #IndigenousStories #IndigenousLanguages &amp; #IndigenousLiteracy. Each year @IndigenousLF gifts culturally appropriate books to remote communities. Get involved &amp; read 'I Saw We Saw' #ILD2019 #IYIL2019 @IYIL2019 https://t.co/YX9sq6W5J1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			285
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IndigenousCulture",
+					"indices": [
+						50,
+						68
+					]
+				},
+				{
+					"text": "IndigenousStories",
+					"indices": [
+						69,
+						87
+					]
+				},
+				{
+					"text": "IndigenousLanguages",
+					"indices": [
+						88,
+						108
+					]
+				},
+				{
+					"text": "IndigenousLiteracy",
+					"indices": [
+						115,
+						134
+					]
+				},
+				{
+					"text": "ILD2019",
+					"indices": [
+						257,
+						265
+					]
+				},
+				{
+					"text": "IYIL2019",
+					"indices": [
+						266,
+						275
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IndigenousLF",
+					"name": "ILF",
+					"id": 332554525,
+					"id_str": "332554525",
+					"indices": [
+						146,
+						159
+					]
+				},
+				{
+					"screen_name": "IYIL2019",
+					"name": "International Year of Indigenous Languages 2019",
+					"id": 1052568338101141500,
+					"id_str": "1052568338101141506",
+					"indices": [
+						276,
+						285
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1168992248794685400,
+					"id_str": "1168992248794685447",
+					"indices": [
+						286,
+						309
+					],
+					"media_url": "http://pbs.twimg.com/media/EDkYQbaXoAczdJF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDkYQbaXoAczdJF.jpg",
+					"url": "https://t.co/YX9sq6W5J1",
+					"display_url": "pic.twitter.com/YX9sq6W5J1",
+					"expanded_url": "https://twitter.com/IndigLandSea/status/1168992250233274374/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 889,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 889,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1168992248794685400,
+					"id_str": "1168992248794685447",
+					"indices": [
+						286,
+						309
+					],
+					"media_url": "http://pbs.twimg.com/media/EDkYQbaXoAczdJF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDkYQbaXoAczdJF.jpg",
+					"url": "https://t.co/YX9sq6W5J1",
+					"display_url": "pic.twitter.com/YX9sq6W5J1",
+					"expanded_url": "https://twitter.com/IndigLandSea/status/1168992250233274374/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 889,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 889,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3112381887,
+			"id_str": "3112381887",
+			"name": "Indigenous Land & Sea Corp",
+			"screen_name": "IndigLandSea",
+			"location": "Australia",
+			"description": "Assisting Indigenous people to realise the economic, social, cultural & environmental benefits that ownership & management of #land & #water can bring.",
+			"url": "https://t.co/Psl7yW9bTC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Psl7yW9bTC",
+							"expanded_url": "http://www.ilsc.gov.au",
+							"display_url": "ilsc.gov.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 830,
+			"friends_count": 854,
+			"listed_count": 7,
+			"created_at": "Wed Mar 25 01:36:05 +0000 2015",
+			"favourites_count": 377,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 554,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1147030529457266688/RGn23qEM_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1147030529457266688/RGn23qEM_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3112381887/1564116125",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It's Indigenous Literacy Day! A time to celebrate #IndigenousCulture #IndigenousStories #IndigenousLanguages &amp; #IndigenousLiteracy. Each year @IndigenousLF gifts culturally appropriate books to remote communities. Get involved &amp; read 'I Saw We Saw' #ILD2019 #IYIL2019 @IYIL2019 https://t.co/YX9sq6W5J1"
+	},
+	{
+		"created_at": "Tue Sep 03 20:12:35 +0000 2019",
+		"id": 1168980128921215000,
+		"id_str": "1168980128921214978",
+		"full_text": "I always enjoy looking at this showcase, as there are interesting new and renovated libraries to look at (which we can't all travel to visit) https://t.co/rHDEAaSiDb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rHDEAaSiDb",
+					"expanded_url": "https://twitter.com/amlibraries/status/1168944004790128643",
+					"display_url": "twitter.com/amlibraries/st…",
+					"indices": [
+						142,
+						165
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2513,
+			"friends_count": 800,
+			"listed_count": 232,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 35910,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1168944004790128600,
+		"quoted_status_id_str": "1168944004790128643",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rHDEAaSiDb",
+			"expanded": "https://twitter.com/amlibraries/status/1168944004790128643",
+			"display": "twitter.com/amlibraries/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 03 17:49:03 +0000 2019",
+			"id": 1168944004790128600,
+			"id_str": "1168944004790128643",
+			"full_text": "See the year's most impressive new and renovated libraries in our annual Library Design Showcase https://t.co/R5oopT8q1w https://t.co/ixoo55mant",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				120
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/R5oopT8q1w",
+						"expanded_url": "http://bit.ly/2NLIyQf",
+						"display_url": "bit.ly/2NLIyQf",
+						"indices": [
+							97,
+							120
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1168944002592313300,
+						"id_str": "1168944002592313349",
+						"indices": [
+							121,
+							144
+						],
+						"media_url": "http://pbs.twimg.com/media/EDjsYITWkAUiGH_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDjsYITWkAUiGH_.jpg",
+						"url": "https://t.co/ixoo55mant",
+						"display_url": "pic.twitter.com/ixoo55mant",
+						"expanded_url": "https://twitter.com/amlibraries/status/1168944004790128643/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 970,
+								"h": 647,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 970,
+								"h": 647,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1168944002592313300,
+						"id_str": "1168944002592313349",
+						"indices": [
+							121,
+							144
+						],
+						"media_url": "http://pbs.twimg.com/media/EDjsYITWkAUiGH_.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDjsYITWkAUiGH_.jpg",
+						"url": "https://t.co/ixoo55mant",
+						"display_url": "pic.twitter.com/ixoo55mant",
+						"expanded_url": "https://twitter.com/amlibraries/status/1168944004790128643/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 970,
+								"h": 647,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 970,
+								"h": 647,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18955776,
+				"id_str": "18955776",
+				"name": "American Libraries",
+				"screen_name": "amlibraries",
+				"location": "Chicago",
+				"description": "Magazine of the American Library Association. Tweeters include Amy Carlton, Terra Dankowski, George Eberhart, Phil Morehart, Sallyann Price & Sanhita SinhaRoy.",
+				"url": "http://t.co/2Dq9yGNmHL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/2Dq9yGNmHL",
+								"expanded_url": "http://americanlibrariesmagazine.org",
+								"display_url": "americanlibrariesmagazine.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 78304,
+				"friends_count": 1106,
+				"listed_count": 2295,
+				"created_at": "Tue Jan 13 22:08:09 +0000 2009",
+				"favourites_count": 699,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 25746,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/472100953567420416/OIkjjhbj_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/472100953567420416/OIkjjhbj_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18955776/1559570298",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 17,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I always enjoy looking at this showcase, as there are interesting new and renovated libraries to look at (which we can't all travel to visit) https://t.co/rHDEAaSiDb"
+	},
+	{
 		"created_at": "Mon Sep 02 20:21:59 +0000 2019",
 		"id": 1168620103434297300,
 		"id_str": "1168620103434297344",
