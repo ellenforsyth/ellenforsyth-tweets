e9ddcf009ecf4ec181c7850ca55fccb107c1e4ca
ellenforsyth <ellenforsyth@gmail.com>
2019-11-02 18:15:02 +1100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36552,
-	"created_at": "Wed Oct 30 07:15:01 +0000 2019",
+	"tweets": 36554,
+	"created_at": "Sat Nov 02 07:15:01 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,518 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 22:14:42 +0000 2019",
+		"id": 1190391740542283800,
+		"id_str": "1190391740542283776",
+		"full_text": "this contains a lot of information relevant to local studies collections too - who is hiding in plain sight in your community? https://t.co/OyfREoMXMf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OyfREoMXMf",
+					"expanded_url": "https://twitter.com/Passion_4/status/1190250894425763845",
+					"display_url": "twitter.com/Passion_4/stat…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2521,
+			"friends_count": 803,
+			"listed_count": 233,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5551,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36080,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1190250894425763800,
+		"quoted_status_id_str": "1190250894425763845",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OyfREoMXMf",
+			"expanded": "https://twitter.com/Passion_4/status/1190250894425763845",
+			"display": "twitter.com/Passion_4/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Nov 01 12:55:01 +0000 2019",
+			"id": 1190250894425763800,
+			"id_str": "1190250894425763845",
+			"full_text": "Hidden in Plain Sight - in honour of Black History Month @lhamiltonbrown guest blog piece…https://t.co/EnXsunfE30",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				113
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "lhamiltonbrown",
+						"name": "Lorna Hamilton-Brown",
+						"id": 107102350,
+						"id_str": "107102350",
+						"indices": [
+							57,
+							72
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/EnXsunfE30",
+						"expanded_url": "https://www.loopknitlounge.com/2019/11/hidden-in-plain-sight-in-honour-of-black-history-month/",
+						"display_url": "loopknitlounge.com/2019/11/hidden…",
+						"indices": [
+							90,
+							113
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 176992897,
+				"id_str": "176992897",
+				"name": "Passion 4",
+				"screen_name": "Passion_4",
+				"location": "near the sea",
+				"description": "Passionate, eclectic, visionary, pencil loving, technology inspired, all served with a fascination of typography, distressed materials and vintage VW campers.",
+				"url": "https://t.co/VNibnA24x7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VNibnA24x7",
+								"expanded_url": "https://www.passion4.co.uk",
+								"display_url": "passion4.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 280,
+				"friends_count": 765,
+				"listed_count": 98,
+				"created_at": "Wed Aug 11 00:03:42 +0000 2010",
+				"favourites_count": 551,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6439,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/958385595071516672/wTOfd5YC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/958385595071516672/wTOfd5YC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/176992897/1471105399",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "this contains a lot of information relevant to local studies collections too - who is hiding in plain sight in your community? https://t.co/OyfREoMXMf"
+	},
+	{
+		"created_at": "Fri Nov 01 22:13:50 +0000 2019",
+		"id": 1190270207522148400,
+		"id_str": "1190391522543296512",
+		"full_text": "This amazing piece by @lhamiltonbrown touches on the invisibility of black knitters in recent, British history, but also mentions the invisibility of the black servicemen and women who served in the Wars and the important commemorative work of @Blackpoppyrose. https://t.co/Pg5Fy31qS0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lhamiltonbrown",
+					"name": "Lorna Hamilton-Brown",
+					"id": 107102350,
+					"id_str": "107102350",
+					"indices": [
+						22,
+						37
+					]
+				},
+				{
+					"screen_name": "Blackpoppyrose",
+					"name": "BlackPoppyRose",
+					"id": 3497190252,
+					"id_str": "3497190252",
+					"indices": [
+						244,
+						259
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Pg5Fy31qS0",
+					"expanded_url": "https://twitter.com/lhamiltonbrown/status/1190200622894735365",
+					"display_url": "twitter.com/lhamiltonbrown…",
+					"indices": [
+						261,
+						284
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18935811,
+			"id_str": "18935811",
+			"name": "Felicity Ford ♥️🇪🇺",
+			"screen_name": "knitsonik",
+			"location": "Reading, UK",
+			"description": "Dismantling white supremacist patriarchal capitalism while knitting. Artist + Knitter. Fan of Missy Elliott. Author of KNITSONIK Sourcebook + Playbook.",
+			"url": "https://t.co/8IiU01kFMq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8IiU01kFMq",
+							"expanded_url": "http://knitsonik.bigcartel.com/",
+							"display_url": "knitsonik.bigcartel.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3393,
+			"friends_count": 1262,
+			"listed_count": 118,
+			"created_at": "Tue Jan 13 11:47:04 +0000 2009",
+			"favourites_count": 14441,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11894,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/971892838026874880/LKE3qtpU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/971892838026874880/LKE3qtpU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18935811/1520552246",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1190200622894735400,
+		"quoted_status_id_str": "1190200622894735365",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Pg5Fy31qS0",
+			"expanded": "https://twitter.com/lhamiltonbrown/status/1190200622894735365",
+			"display": "twitter.com/lhamiltonbrown…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Nov 01 09:35:16 +0000 2019",
+			"id": 1190200622894735400,
+			"id_str": "1190200622894735365",
+			"full_text": "I’ve written a BHM guest blog post for @loopknitting https://t.co/2jFdH6ztuE describing how women in the Caribbean knitted for the war effort and how important #andrealevy Small Island is in exposing the hidden stories of Jamaican knitters. @Dorcasstories @Diversehistory2",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "andrealevy",
+						"indices": [
+							160,
+							171
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "loopknitting",
+						"name": "Loop",
+						"id": 20660215,
+						"id_str": "20660215",
+						"indices": [
+							39,
+							52
+						]
+					},
+					{
+						"screen_name": "Dorcasstories",
+						"name": "Rose Sinclair",
+						"id": 4756485016,
+						"id_str": "4756485016",
+						"indices": [
+							241,
+							255
+						]
+					},
+					{
+						"screen_name": "Diversehistory2",
+						"name": "Diverse Histories +",
+						"id": 4145350697,
+						"id_str": "4145350697",
+						"indices": [
+							256,
+							272
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/2jFdH6ztuE",
+						"expanded_url": "https://bit.ly/324btCE",
+						"display_url": "bit.ly/324btCE",
+						"indices": [
+							53,
+							76
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 107102350,
+				"id_str": "107102350",
+				"name": "Lorna Hamilton-Brown",
+				"screen_name": "lhamiltonbrown",
+				"location": "By the sea",
+				"description": "Illustrator, Knitting artist,  Entrepreneur, Employability Tutor, Motivational Speaker, Talent spotter, Introducer, Mentor and much more :-)",
+				"url": "http://t.co/XNgKt9i4xm",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/XNgKt9i4xm",
+								"expanded_url": "http://www.lornahamiltonbrown.com",
+								"display_url": "lornahamiltonbrown.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1028,
+				"friends_count": 1325,
+				"listed_count": 79,
+				"created_at": "Thu Jan 21 15:18:06 +0000 2010",
+				"favourites_count": 2482,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 6642,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/569160309684715520/eHXvEOCl_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/569160309684715520/eHXvEOCl_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/107102350/1424535843",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 25,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This amazing piece by @lhamiltonbrown touches on the invisibility of black knitters in recent, British history, but also mentions the invisibility of the black servicemen and women who served in the Wars and the important commemorative work of @Blackpoppyrose. https://t.co/Pg5Fy31qS0"
+	}
+]
