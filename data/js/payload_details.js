a48fd5b738323b00018c2a92d8dd3a81f869452f
ellenforsyth <ellenforsyth@gmail.com>
2019-12-02 18:15:03 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36661,
-	"created_at": "Sun Dec 01 07:15:02 +0000 2019",
+	"tweets": 36662,
+	"created_at": "Mon Dec 02 07:15:02 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,132 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 08:58:41 +0000 2019",
+		"id": 1200354034235969500,
+		"id_str": "1201063055750156290",
+		"full_text": ".@mollyblackall @Gdntakepart: Help us locate Benin bronze statues in the UK https://t.co/65raAPybT2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mollyblackall",
+					"name": "Molly Blackall",
+					"id": 1086029539501883400,
+					"id_str": "1086029539501883394",
+					"indices": [
+						1,
+						15
+					]
+				},
+				{
+					"screen_name": "Gdntakepart",
+					"name": "Guardian Community",
+					"id": 1073418025,
+					"id_str": "1073418025",
+					"indices": [
+						16,
+						28
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/65raAPybT2",
+					"expanded_url": "https://conflictantiquities.wordpress.com/2019/11/29/uk-1897-punitive-expedition-benin-crowdsourcing/",
+					"display_url": "conflictantiquities.wordpress.com/2019/11/29/uk-…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1029069799,
+			"id_str": "1029069799",
+			"name": "Conflict Antiquities",
+			"screen_name": "conflictantiq",
+			"location": "Rome, Lazio",
+			"description": "Post-Doctoral Research Fellow in Cultural Heritage and Conflicts @norskeinstitutt @UniOslo",
+			"url": "http://t.co/Q0SJQFeI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/Q0SJQFeI",
+							"expanded_url": "http://conflictantiquities.wordpress.com",
+							"display_url": "conflictantiquities.wordpress.com",
+							"indices": [
+								0,
+								20
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7033,
+			"friends_count": 2807,
+			"listed_count": 217,
+			"created_at": "Sat Dec 22 18:50:34 +0000 2012",
+			"favourites_count": 3738,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6667,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3009019552/c3ee792d373eba24d57862e62c357fdb_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3009019552/c3ee792d373eba24d57862e62c357fdb_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1029069799/1359781891",
+			"profile_link_color": "D02B55",
+			"profile_sidebar_border_color": "829D5E",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".@mollyblackall @Gdntakepart: Help us locate Benin bronze statues in the UK https://t.co/65raAPybT2"
+	}
+]
