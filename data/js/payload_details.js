39364f6e65c880c5109ee2b108abd55f98fcfccc
ellenforsyth <ellenforsyth@gmail.com>
2019-12-05 18:15:02 +1100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36663,
-	"created_at": "Wed Dec 04 07:15:00 +0000 2019",
+	"tweets": 36667,
+	"created_at": "Thu Dec 05 07:15:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,566 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Wed Dec 04 20:01:20 +0000 2019",
+		"id": 1199797487894225000,
+		"id_str": "1202316979538784256",
+		"full_text": "Love love this WaPo op-ed by Common Threads Project founder Rachel Cohen on the good work they’re doing: https://t.co/7IdMaDlpT4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7IdMaDlpT4",
+					"expanded_url": "https://www.washingtonpost.com/opinions/2019/11/27/some-trauma-really-is-unspeakable-so-these-women-are-sewing-their-stories-instead/",
+					"display_url": "washingtonpost.com/opinions/2019/…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17848020,
+			"id_str": "17848020",
+			"name": "Betsy Greer",
+			"screen_name": "craftivista",
+			"location": "Durham, NC",
+			"description": "#Craftivist since 2003. Writer. Affirmation stitcher (#yasvb). Joy seeker. Toast enthusiast. she/her betsy@hellobetsygreer.com",
+			"url": "https://t.co/9tYmFlMtuR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9tYmFlMtuR",
+							"expanded_url": "http://linktr.ee/craftivista",
+							"display_url": "linktr.ee/craftivista",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4785,
+			"friends_count": 2178,
+			"listed_count": 197,
+			"created_at": "Wed Dec 03 20:41:19 +0000 2008",
+			"favourites_count": 5120,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16923,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/615712979192033280/Cb9ockBS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/615712979192033280/Cb9ockBS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17848020/1495838272",
+			"profile_link_color": "0ED2F5",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Love love this WaPo op-ed by Common Threads Project founder Rachel Cohen on the good work they’re doing: https://t.co/7IdMaDlpT4"
+	},
+	{
+		"created_at": "Wed Dec 04 19:56:35 +0000 2019",
+		"id": 1202315783444914200,
+		"id_str": "1202315783444914176",
+		"full_text": "Children of the fallen: 14 Portraits of Loss https://t.co/ekS8CKxDDL via @washingtonpost",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "washingtonpost",
+					"name": "The Washington Post",
+					"id": 2467791,
+					"id_str": "2467791",
+					"indices": [
+						73,
+						88
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ekS8CKxDDL",
+					"expanded_url": "http://wapo.st/portraitsofloss",
+					"display_url": "wapo.st/portraitsofloss",
+					"indices": [
+						45,
+						68
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, games, reading, content creation, making/makers, stories, and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2521,
+			"friends_count": 806,
+			"listed_count": 232,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5599,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36198,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Children of the fallen: 14 Portraits of Loss https://t.co/ekS8CKxDDL via @washingtonpost"
+	},
+	{
+		"created_at": "Wed Dec 04 19:49:09 +0000 2019",
+		"id": 1201653299524423700,
+		"id_str": "1202313911669682176",
+		"full_text": "NEW REPORT: ‘Dangerous Summer: Escalating Bushfire, Heat and Drought Risk'\n\nThis summer is shaping up as a terrible trifecta of heatwaves, drought and bushfires, made worse by climate change. We urgently need to reduce our greenhouse gas emissions. https://t.co/PngveLzZhG https://t.co/crPgM2ncyM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PngveLzZhG",
+					"expanded_url": "https://climc.nl/2RdDeaj",
+					"display_url": "climc.nl/2RdDeaj",
+					"indices": [
+						249,
+						272
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1201653283229585400,
+					"id_str": "1201653283229585409",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/EK0hS2IU0AEGEt9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EK0hS2IU0AEGEt9.jpg",
+					"url": "https://t.co/crPgM2ncyM",
+					"display_url": "pic.twitter.com/crPgM2ncyM",
+					"expanded_url": "https://twitter.com/climatecouncil/status/1201653299524423680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1201653283229585400,
+					"id_str": "1201653283229585409",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/EK0hS2IU0AEGEt9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EK0hS2IU0AEGEt9.jpg",
+					"url": "https://t.co/crPgM2ncyM",
+					"display_url": "pic.twitter.com/crPgM2ncyM",
+					"expanded_url": "https://twitter.com/climatecouncil/status/1201653299524423680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1892683062,
+			"id_str": "1892683062",
+			"name": "Climate Council",
+			"screen_name": "climatecouncil",
+			"location": "Australia",
+			"description": "Providing Australians with a reliable source of information on climate science.",
+			"url": "https://t.co/UGPCNnqynl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UGPCNnqynl",
+							"expanded_url": "http://linktr.ee/climatecouncil",
+							"display_url": "linktr.ee/climatecouncil",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 48986,
+			"friends_count": 17685,
+			"listed_count": 991,
+			"created_at": "Sun Sep 22 06:29:31 +0000 2013",
+			"favourites_count": 1863,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18533,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/504862206479970304/rWWL00qW_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/504862206479970304/rWWL00qW_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1892683062/1572313559",
+			"profile_link_color": "EFAB23",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 49,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "NEW REPORT: ‘Dangerous Summer: Escalating Bushfire, Heat and Drought Risk'\n\nThis summer is shaping up as a terrible trifecta of heatwaves, drought and bushfires, made worse by climate change. We urgently need to reduce our greenhouse gas emissions. https://t.co/PngveLzZhG https://t.co/crPgM2ncyM"
+	},
+	{
+		"created_at": "Wed Dec 04 18:46:23 +0000 2019",
+		"id": 1202293827194671000,
+		"id_str": "1202298116453486592",
+		"full_text": "The bold pioneers of cinema who paved the way--including The Queens of Animation  https://t.co/26m7q70FOA via @BBC_Culture",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBC_Culture",
+					"name": "BBC Culture",
+					"id": 1207654748,
+					"id_str": "1207654748",
+					"indices": [
+						110,
+						122
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/26m7q70FOA",
+					"expanded_url": "http://www.bbc.com/culture/story/20191203-the-bold-pioneers-of-cinema-who-paved-the-way?ocid=ww.social.link.twitter",
+					"display_url": "bbc.com/culture/story/…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 460547107,
+			"id_str": "460547107",
+			"name": "Nathalia Holt",
+			"screen_name": "NathaliaHolt",
+			"location": "Pacific Grove, CA",
+			"description": "NYT bestselling author of RISE OF THE ROCKET GIRLS, CURED, & THE QUEENS OF ANIMATION out now!",
+			"url": "https://t.co/pUqdA0Qaxf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pUqdA0Qaxf",
+							"expanded_url": "http://www.nathaliaholt.com",
+							"display_url": "nathaliaholt.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3710,
+			"friends_count": 816,
+			"listed_count": 137,
+			"created_at": "Tue Jan 10 21:43:12 +0000 2012",
+			"favourites_count": 2067,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4326,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000821318296/12689eb3e9323c163b4238cb0ff37524_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000821318296/12689eb3e9323c163b4238cb0ff37524_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/460547107/1567527206",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The bold pioneers of cinema who paved the way--including The Queens of Animation  https://t.co/26m7q70FOA via @BBC_Culture"
+	},
+	{
 		"created_at": "Tue Dec 03 21:11:47 +0000 2019",
 		"id": 1201972319565930500,
 		"id_str": "1201972319565930496",
