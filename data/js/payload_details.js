b207f032eea8096ac7f2b4bb94841196071121bd
ellenforsyth <ellenforsyth@gmail.com>
2020-03-05 18:15:05 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36960,
-	"created_at": "Wed Mar 04 07:15:01 +0000 2020",
+	"tweets": 36961,
+	"created_at": "Thu Mar 05 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,272 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 04 07:21:22 +0000 2020",
+		"id": 1234720640009064400,
+		"id_str": "1235103021442273281",
+		"full_text": "We’re also looking for a PhD student to study Brush-turkey behavioural adaptation to urban areas\nhttps://t.co/RR5hynb6gP\n\nThis is a 3-month old chick, having survived this long with no parental care\n\n@dieterhochuli @alburns1 @SydneySOLES @Sydney_Science @tarongazoo https://t.co/92adHcRGrQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dieterhochuli",
+					"name": "Dieter Hochuli",
+					"id": 15901734,
+					"id_str": "15901734",
+					"indices": [
+						200,
+						214
+					]
+				},
+				{
+					"screen_name": "alburns1",
+					"name": "Alicia Burns",
+					"id": 2829301334,
+					"id_str": "2829301334",
+					"indices": [
+						215,
+						224
+					]
+				},
+				{
+					"screen_name": "SydneySOLES",
+					"name": "Sydney Uni Life&Env",
+					"id": 116887260,
+					"id_str": "116887260",
+					"indices": [
+						225,
+						237
+					]
+				},
+				{
+					"screen_name": "Sydney_Science",
+					"name": "Sydney Science",
+					"id": 1963897189,
+					"id_str": "1963897189",
+					"indices": [
+						238,
+						253
+					]
+				},
+				{
+					"screen_name": "tarongazoo",
+					"name": "Taronga Zoo",
+					"id": 17773366,
+					"id_str": "17773366",
+					"indices": [
+						254,
+						265
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RR5hynb6gP",
+					"expanded_url": "https://www.ecolsoc.org.au/opportunities/2020/phd-brush-turkey-behavioural-adaptation",
+					"display_url": "ecolsoc.org.au/opportunities/…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1234720596061114400,
+					"id_str": "1234720596061114369",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1234720596061114369/pu/img/mZGXQk-BUfKlrIqb.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1234720596061114369/pu/img/mZGXQk-BUfKlrIqb.jpg",
+					"url": "https://t.co/92adHcRGrQ",
+					"display_url": "pic.twitter.com/92adHcRGrQ",
+					"expanded_url": "https://twitter.com/Wingtags/status/1234720640009064449/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1234720596061114400,
+					"id_str": "1234720596061114369",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1234720596061114369/pu/img/mZGXQk-BUfKlrIqb.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1234720596061114369/pu/img/mZGXQk-BUfKlrIqb.jpg",
+					"url": "https://t.co/92adHcRGrQ",
+					"display_url": "pic.twitter.com/92adHcRGrQ",
+					"expanded_url": "https://twitter.com/Wingtags/status/1234720640009064449/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 21898,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1234720596061114369/pu/vid/1280x720/6mHFD1_tfWX7e3fL.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1234720596061114369/pu/vid/480x270/QoFg_lkMoRJixwop.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1234720596061114369/pu/vid/640x360/NXmFHEVHgrzObs-x.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1234720596061114369/pu/pl/J-rjw2T29Me_Y1If.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1234718211108528000,
+		"in_reply_to_status_id_str": "1234718211108528129",
+		"in_reply_to_user_id": 620761847,
+		"in_reply_to_user_id_str": "620761847",
+		"in_reply_to_screen_name": "Wingtags",
+		"user": {
+			"id": 620761847,
+			"id_str": "620761847",
+			"name": "John Martin",
+			"screen_name": "Wingtags",
+			"location": "Sydney, Australia",
+			"description": "“We are responsible for the world’s health, not it for ours”                Wayward Ecology, Bin-Chicken, Clever Cockie, Turkey tsunami, habitat, @HollowsAsHome",
+			"url": "https://t.co/yKkIwMqzp7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yKkIwMqzp7",
+							"expanded_url": "https://linktr.ee/wingtagsandhollows",
+							"display_url": "linktr.ee/wingtagsandhol…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3632,
+			"friends_count": 851,
+			"listed_count": 99,
+			"created_at": "Thu Jun 28 07:50:43 +0000 2012",
+			"favourites_count": 15036,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8614,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1145438690375716864/mlcQ9-Oq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1145438690375716864/mlcQ9-Oq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/620761847/1400225396",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 30,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We’re also looking for a PhD student to study Brush-turkey behavioural adaptation to urban areas\nhttps://t.co/RR5hynb6gP\n\nThis is a 3-month old chick, having survived this long with no parental care\n\n@dieterhochuli @alburns1 @SydneySOLES @Sydney_Science @tarongazoo https://t.co/92adHcRGrQ"
+	},
+	{
 		"created_at": "Tue Mar 03 09:24:41 +0000 2020",
 		"id": 1234758019738722300,
 		"id_str": "1234771668045713409",
