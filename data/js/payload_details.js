70f60f77071bd935791aa099785aaa4d9341aae0
ellenforsyth <ellenforsyth@gmail.com>
2020-03-15 18:15:04 +1100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36977,
-	"created_at": "Sat Mar 14 07:15:01 +0000 2020",
+	"tweets": 36979,
+	"created_at": "Sun Mar 15 07:15:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 15 07:01:07 +0000 2020",
+		"id": 1239084192241037300,
+		"id_str": "1239084192241037314",
+		"full_text": "Your coronavirus reading list: reader suggestions to bring joy in difficult times https://t.co/AD1MMsca1b via @guardian there is a place for libraries to be offering #readadv at these times too",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			193
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "readadv",
+					"indices": [
+						166,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "guardian",
+					"name": "The Guardian",
+					"id": 87818409,
+					"id_str": "87818409",
+					"indices": [
+						110,
+						119
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AD1MMsca1b",
+					"expanded_url": "https://www.theguardian.com/books/2020/mar/13/your-coronavirus-reading-list-reader-suggestions-to-bring-joy-in-difficult-times?CMP=share_btn_tw",
+					"display_url": "theguardian.com/books/2020/mar…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2523,
+			"friends_count": 836,
+			"listed_count": 234,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 5919,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36555,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Your coronavirus reading list: reader suggestions to bring joy in difficult times https://t.co/AD1MMsca1b via @guardian there is a place for libraries to be offering #readadv at these times too"
+	},
+	{
+		"created_at": "Sun Mar 15 05:55:52 +0000 2020",
+		"id": 1239049179621503000,
+		"id_str": "1239067773696765954",
+		"full_text": "We've seen the memes, but WHEN should we wash our hands?\n\n🤧 if you sneeze or cough\n🤒 if in contact with sick people\n🚽 after the loo\n🍲 when eating/cooking\n🐶 if touching/feed pets\n🚌 after visiting public spaces\n\nSoap &amp; water FTW! 🤝🧼😁\n\nPlease share: https://t.co/9RAJd9weL1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9RAJd9weL1",
+					"expanded_url": "http://ab.co/39SsrJ4",
+					"display_url": "ab.co/39SsrJ4",
+					"indices": [
+						251,
+						274
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24646005,
+			"id_str": "24646005",
+			"name": "ABC Science",
+			"screen_name": "ABCscience",
+			"location": "Australia",
+			"description": "Science | Health | Technology | Environment. This is an official @ABCaustralia account.",
+			"url": "https://t.co/DDyAHDyFMX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DDyAHDyFMX",
+							"expanded_url": "https://ab.co/2P8ZwdM",
+							"display_url": "ab.co/2P8ZwdM",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57035,
+			"friends_count": 260,
+			"listed_count": 1238,
+			"created_at": "Mon Mar 16 03:53:52 +0000 2009",
+			"favourites_count": 1264,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 19029,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/989378300932833281/7uq966TA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/989378300932833281/7uq966TA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24646005/1583295323",
+			"profile_link_color": "050505",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "120F15",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 35,
+		"favorite_count": 30,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We've seen the memes, but WHEN should we wash our hands?\n\n🤧 if you sneeze or cough\n🤒 if in contact with sick people\n🚽 after the loo\n🍲 when eating/cooking\n🐶 if touching/feed pets\n🚌 after visiting public spaces\n\nSoap &amp; water FTW! 🤝🧼😁\n\nPlease share: https://t.co/9RAJd9weL1"
+	},
+	{
 		"created_at": "Fri Mar 13 22:39:22 +0000 2020",
 		"id": 1238595535793033200,
 		"id_str": "1238595535793033216",
