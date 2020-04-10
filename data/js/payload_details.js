8be9ec596014f0f1b0760849d80baead6403ff90
ellenforsyth <ellenforsyth@gmail.com>
2020-04-10 17:15:03 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37082,
-	"created_at": "Thu Apr 09 07:15:01 +0000 2020",
+	"tweets": 37083,
+	"created_at": "Fri Apr 10 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 10 04:25:02 +0000 2020",
+		"id": 1248355001350516700,
+		"id_str": "1248466997131673606",
+		"full_text": "Start the day right! Boil the kettle, grab a seat and enjoy a #CuppawiththeBirds. Take 10 mins out of your day - watch the birds in your garden and share who's there. Let’s get a communal #BirdingAtHome list together.\n\nTag a friend you'd love to share a #CuppawiththeBirds with https://t.co/vlUbwqzhAK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CuppawiththeBirds",
+					"indices": [
+						62,
+						80
+					]
+				},
+				{
+					"text": "BirdingAtHome",
+					"indices": [
+						188,
+						202
+					]
+				},
+				{
+					"text": "CuppawiththeBirds",
+					"indices": [
+						254,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1248196929076359200,
+					"id_str": "1248196929076359169",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/EVJ8fmdUcAEccZn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVJ8fmdUcAEccZn.jpg",
+					"url": "https://t.co/vlUbwqzhAK",
+					"display_url": "pic.twitter.com/vlUbwqzhAK",
+					"expanded_url": "https://twitter.com/UrbanBirdsOz/status/1248355001350516736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1772,
+							"h": 1772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1248196929076359200,
+					"id_str": "1248196929076359169",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/EVJ8fmdUcAEccZn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVJ8fmdUcAEccZn.jpg",
+					"url": "https://t.co/vlUbwqzhAK",
+					"display_url": "pic.twitter.com/vlUbwqzhAK",
+					"expanded_url": "https://twitter.com/UrbanBirdsOz/status/1248355001350516736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1772,
+							"h": 1772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 927695714116710400,
+			"id_str": "927695714116710406",
+			"name": "Birds in Backyards",
+			"screen_name": "UrbanBirdsOz",
+			"location": "Australia",
+			"description": "BirdLife Australia \nInsta: @birdsinbackyards\nFb: BirdsinBackyards\nYouTube: https://t.co/8oWRDL7sty \nBIBY, Powerful Owl, Birds in Schools, Bird Strike, Budding Birdos",
+			"url": "https://t.co/CJVqgdXqKk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CJVqgdXqKk",
+							"expanded_url": "http://www.birdsinbackyards.net",
+							"display_url": "birdsinbackyards.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8oWRDL7sty",
+							"expanded_url": "http://tiny.cc/kfjqbz",
+							"display_url": "tiny.cc/kfjqbz",
+							"indices": [
+								75,
+								98
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 764,
+			"friends_count": 210,
+			"listed_count": 5,
+			"created_at": "Tue Nov 07 00:34:28 +0000 2017",
+			"favourites_count": 744,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 315,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234281414037606400/5GEi1Xeh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234281414037606400/5GEi1Xeh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/927695714116710406/1566794412",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 42,
+		"favorite_count": 95,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Start the day right! Boil the kettle, grab a seat and enjoy a #CuppawiththeBirds. Take 10 mins out of your day - watch the birds in your garden and share who's there. Let’s get a communal #BirdingAtHome list together.\n\nTag a friend you'd love to share a #CuppawiththeBirds with https://t.co/vlUbwqzhAK"
+	},
+	{
 		"created_at": "Thu Apr 09 02:06:38 +0000 2020",
 		"id": 1247872336667332600,
 		"id_str": "1248069781489516544",
