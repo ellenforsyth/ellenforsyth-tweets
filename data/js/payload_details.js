a6120151d69ec963eb66f1e6088165d7cd04636c
ellenforsyth <ellenforsyth@gmail.com>
2020-04-13 17:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37095,
-	"created_at": "Sun Apr 12 07:15:02 +0000 2020",
+	"tweets": 37096,
+	"created_at": "Mon Apr 13 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 12 23:20:22 +0000 2020",
+		"id": 1247745173724885000,
+		"id_str": "1249477489648234496",
+		"full_text": "The power of Australian research 🇦🇺💪\n\nIn this thread, we would like to highlight all the different studies happening across Australia which have been contributing to our fight to stop the COVID-19 spread in Australia.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			217
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1242310548881961000,
+			"id_str": "1242310548881960960",
+			"name": "covid-19-au 🦠📈",
+			"screen_name": "covid19augithub",
+			"location": "Melbourne",
+			"description": "A team of student volunteers dedicated to informing the public about COVID-19 dev in 🇦🇺. Visit https://t.co/5qniYIYdxf to see our real time reports 📈📊!",
+			"url": "https://t.co/fbAbMh560Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fbAbMh560Q",
+							"expanded_url": "https://covid-19-au.com/",
+							"display_url": "covid-19-au.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/5qniYIYdxf",
+							"expanded_url": "http://covid-19-au.com",
+							"display_url": "covid-19-au.com",
+							"indices": [
+								95,
+								118
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1258,
+			"friends_count": 51,
+			"listed_count": 9,
+			"created_at": "Tue Mar 24 04:41:41 +0000 2020",
+			"favourites_count": 245,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 135,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1243868059418255365/NPFpyGBX_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1243868059418255365/NPFpyGBX_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1242310548881960960/1585396265",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 32,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The power of Australian research 🇦🇺💪\n\nIn this thread, we would like to highlight all the different studies happening across Australia which have been contributing to our fight to stop the COVID-19 spread in Australia."
+	},
+	{
 		"created_at": "Sat Apr 11 20:38:05 +0000 2020",
 		"id": 1249074260372369400,
 		"id_str": "1249074260372369408",
