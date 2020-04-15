73b08c4498991d8b481bdaef08ad792061a7ed34
ellenforsyth <ellenforsyth@gmail.com>
2020-04-15 17:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37099,
-	"created_at": "Tue Apr 14 07:15:02 +0000 2020",
+	"tweets": 37100,
+	"created_at": "Wed Apr 15 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 14 21:46:59 +0000 2020",
+		"id": 1250178763297452000,
+		"id_str": "1250178763297452032",
+		"full_text": "Why You Should Start a Coronavirus Diary https://t.co/Xmrp1JBGha via @nytimes (and could be something to consider for local studies as well)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nytimes",
+					"name": "The New York Times",
+					"id": 807095,
+					"id_str": "807095",
+					"indices": [
+						69,
+						77
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Xmrp1JBGha",
+					"expanded_url": "https://www.nytimes.com/2020/04/13/smarter-living/why-you-should-start-a-coronavirus-diary.html?smid=tw-share",
+					"display_url": "nytimes.com/2020/04/13/sma…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2529,
+			"friends_count": 847,
+			"listed_count": 234,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 6138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36699,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Why You Should Start a Coronavirus Diary https://t.co/Xmrp1JBGha via @nytimes (and could be something to consider for local studies as well)"
+	},
+	{
 		"created_at": "Mon Apr 13 22:33:56 +0000 2020",
 		"id": 1249102548516008000,
 		"id_str": "1249828191197212672",
@@ -291,7 +408,7 @@ Grailbird.data.tweets_2020_04 =
 			"name": "Sarah Mirk",
 			"screen_name": "sarahmirk",
 			"location": "Portland, OR",
-			"description": "Comics + journalism. Web producer @reveal. Other work: @thenib & @BitchMedia. Obsessive zine-maker & writer of an illustrated oral history of GITMO. 🏳️‍🌈🌯🔥",
+			"description": "Comics + journalism. Web producer @reveal. Other work: @thenib & @BitchMedia. Obsessive zine-maker & writer of an illustrated oral history of GITMO. 🏳️🌈🌯🔥",
 			"url": "https://t.co/NigfOwJERP",
 			"entities": {
 				"url": {
