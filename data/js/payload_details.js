97b2155aafa9572c70d52b3d5f9505c827975151
ellenforsyth <ellenforsyth@gmail.com>
2020-06-10 17:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37221,
-	"created_at": "Mon Jun 08 07:15:01 +0000 2020",
+	"tweets": 37222,
+	"created_at": "Wed Jun 10 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 09 11:10:38 +0000 2020",
+		"id": 1270153192412795000,
+		"id_str": "1270312343252033536",
+		"full_text": "#BlackLivesMatterAustralia protesters referred to our count of 432 Aboriginal deaths in custody. It's now 437 #DeathsInside.\n\n#IndigenousLivesMatter #AboriginalLivesMatter #BLM #BlackLivesMatter https://t.co/7e95QTczfB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BlackLivesMatterAustralia",
+					"indices": [
+						0,
+						26
+					]
+				},
+				{
+					"text": "DeathsInside",
+					"indices": [
+						110,
+						123
+					]
+				},
+				{
+					"text": "IndigenousLivesMatter",
+					"indices": [
+						126,
+						148
+					]
+				},
+				{
+					"text": "AboriginalLivesMatter",
+					"indices": [
+						149,
+						171
+					]
+				},
+				{
+					"text": "BLM",
+					"indices": [
+						172,
+						176
+					]
+				},
+				{
+					"text": "BlackLivesMatter",
+					"indices": [
+						177,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7e95QTczfB",
+					"expanded_url": "https://www.theguardian.com/australia-news/2020/jun/09/black-lives-matter-protesters-referred-to-our-count-of-432-aboriginal-deaths-in-custody-its-now-437?CMP=soc_568&utm_medium=Social&utm_source=Twitter#Echobox=1591663092",
+					"display_url": "theguardian.com/australia-news…",
+					"indices": [
+						195,
+						218
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1092378031,
+			"id_str": "1092378031",
+			"name": "Guardian Australia",
+			"screen_name": "GuardianAus",
+			"location": "Australia",
+			"description": "Latest news from Guardian Australia",
+			"url": "https://t.co/yhI6Vqsr3q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yhI6Vqsr3q",
+							"expanded_url": "https://support.theguardian.com/au/contribute",
+							"display_url": "support.theguardian.com/au/contribute",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 266444,
+			"friends_count": 161,
+			"listed_count": 2450,
+			"created_at": "Tue Jan 15 15:16:44 +0000 2013",
+			"favourites_count": 101,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 222785,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2AFA9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1175902409534476288/iQwEZavm_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1175902409534476288/iQwEZavm_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1092378031/1569228481",
+			"profile_link_color": "005789",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 43,
+		"favorite_count": 52,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BlackLivesMatterAustralia protesters referred to our count of 432 Aboriginal deaths in custody. It's now 437 #DeathsInside.\n\n#IndigenousLivesMatter #AboriginalLivesMatter #BLM #BlackLivesMatter https://t.co/7e95QTczfB"
+	},
+	{
 		"created_at": "Mon Jun 08 02:57:44 +0000 2020",
 		"id": 1268984203821092900,
 		"id_str": "1269825911060226048",
