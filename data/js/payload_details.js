0a52bb51851aafc28f4b13723583508073d3b5ba
ellenforsyth <ellenforsyth@gmail.com>
2020-06-16 17:15:05 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37234,
-	"created_at": "Sun Jun 14 07:15:01 +0000 2020",
+	"tweets": 37235,
+	"created_at": "Tue Jun 16 07:15:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 15 21:41:51 +0000 2020",
+		"id": 1272423285930811400,
+		"id_str": "1272645520280842240",
+		"full_text": "⁣Netflix is giving you 13 Reasons Why to watch their show. But we're giving you one to go outside. And contribute to science. 🎥⠀\n⠀\nBush Blitz is Australia’s largest species discovery program. But this year, they want your help! @atlaslivingaust \n\nhttps://t.co/s66wTqlMS9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "atlaslivingaust",
+					"name": "Atlas of Living Aust",
+					"id": 91461955,
+					"id_str": "91461955",
+					"indices": [
+						228,
+						244
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/s66wTqlMS9",
+					"expanded_url": "https://blog.csiro.au/bush-blitz-at-home/",
+					"display_url": "blog.csiro.au/bush-blitz-at-…",
+					"indices": [
+						247,
+						270
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 174083824,
+			"id_str": "174083824",
+			"name": "CSIRO",
+			"screen_name": "CSIRO",
+			"location": "Australia",
+			"description": "We ♥ science. We're Australia's national science agency. We solve the greatest challenges through innovative science and technology. RTs not endorsements.",
+			"url": "https://t.co/uQjDbxq1tI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uQjDbxq1tI",
+							"expanded_url": "http://www.csiro.au/",
+							"display_url": "csiro.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97861,
+			"friends_count": 1743,
+			"listed_count": 1558,
+			"created_at": "Tue Aug 03 02:06:21 +0000 2010",
+			"favourites_count": 4454,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 27183,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "02A9CE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1235684634026848256/u3nJZOPS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1235684634026848256/u3nJZOPS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174083824/1591225901",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E3FFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "⁣Netflix is giving you 13 Reasons Why to watch their show. But we're giving you one to go outside. And contribute to science. 🎥⠀\n⠀\nBush Blitz is Australia’s largest species discovery program. But this year, they want your help! @atlaslivingaust \n\nhttps://t.co/s66wTqlMS9"
+	},
+	{
 		"created_at": "Sat Jun 13 20:06:53 +0000 2020",
 		"id": 1271896846764368000,
 		"id_str": "1271896846764367872",
