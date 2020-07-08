16cb5105adc7d892b8601c0961908dd8e363bd66
ellenforsyth <ellenforsyth@gmail.com>
2020-07-08 17:15:04 +1000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37261,
-	"created_at": "Tue Jul 07 07:15:00 +0000 2020",
+	"tweets": 37264,
+	"created_at": "Wed Jul 08 07:15:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,395 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Wed Jul 08 04:35:39 +0000 2020",
+		"id": 1280722190460227600,
+		"id_str": "1280722190460227584",
+		"full_text": "@SueWilson_235 @cfwriter @Sydney_Uni @ArtSS_Sydney Congratulations 🐉",
+		"truncated": false,
+		"display_text_range": [
+			51,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SueWilson_235",
+					"name": "TweetSue",
+					"id": 434377253,
+					"id_str": "434377253",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "cfwriter",
+					"name": "Dr Rachel Franks",
+					"id": 52325959,
+					"id_str": "52325959",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"screen_name": "Sydney_Uni",
+					"name": "University of Sydney",
+					"id": 20104025,
+					"id_str": "20104025",
+					"indices": [
+						25,
+						36
+					]
+				},
+				{
+					"screen_name": "ArtSS_Sydney",
+					"name": "ArtSS_Sydney",
+					"id": 466195192,
+					"id_str": "466195192",
+					"indices": [
+						37,
+						50
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1280702715895541800,
+		"in_reply_to_status_id_str": "1280702715895541762",
+		"in_reply_to_user_id": 434377253,
+		"in_reply_to_user_id_str": "434377253",
+		"in_reply_to_screen_name": "SueWilson_235",
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2526,
+			"friends_count": 865,
+			"listed_count": 230,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 6489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SueWilson_235 @cfwriter @Sydney_Uni @ArtSS_Sydney Congratulations 🐉"
+	},
+	{
+		"created_at": "Tue Jul 07 21:39:21 +0000 2020",
+		"id": 1280415386853355500,
+		"id_str": "1280617424472031232",
+		"full_text": "\"Poetry has always been a way of articulating anger, and ideas about injustice and the struggle against it. It was always the cultural dimension of what I was doing on the streets, the demonstration, the picket line.\" #PENPinterPrize\nhttps://t.co/heBMG1qp5x",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PENPinterPrize",
+					"indices": [
+						218,
+						233
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/heBMG1qp5x",
+					"expanded_url": "https://blogs.bl.uk/sound-and-vision/2020/07/linton-kwesi-johnson-awarded-pen-pinter-prize-2020.html",
+					"display_url": "blogs.bl.uk/sound-and-visi…",
+					"indices": [
+						234,
+						257
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2827421813,
+			"id_str": "2827421813",
+			"name": "British Library Oral History",
+			"screen_name": "BL_OralHistory",
+			"location": "London",
+			"description": "Posts from British Library oral history department staff & National Life Stories, an oral history fieldwork charity run out of the BL.",
+			"url": "https://t.co/KuQytPEVhU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KuQytPEVhU",
+							"expanded_url": "http://www.bl.uk/subjects/oral-history",
+							"display_url": "bl.uk/subjects/oral-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3799,
+			"friends_count": 722,
+			"listed_count": 62,
+			"created_at": "Mon Oct 13 13:28:53 +0000 2014",
+			"favourites_count": 3484,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3810,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/521654189399609347/BQl7LL2K_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/521654189399609347/BQl7LL2K_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2827421813/1413207133",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Poetry has always been a way of articulating anger, and ideas about injustice and the struggle against it. It was always the cultural dimension of what I was doing on the streets, the demonstration, the picket line.\" #PENPinterPrize\nhttps://t.co/heBMG1qp5x"
+	},
+	{
+		"created_at": "Tue Jul 07 21:38:31 +0000 2020",
+		"id": 1280478452915810300,
+		"id_str": "1280617215834746880",
+		"full_text": "New blog post for @BL_DigiSchol, 'Readings at the intersection of digital scholarship and anti-racism' looks back at topics we've discussed to understand the inequalities and biases that tools and data can add to #DigitalHumanities #DigHist #MuseTech work https://t.co/prmyOIwLff",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigitalHumanities",
+					"indices": [
+						213,
+						231
+					]
+				},
+				{
+					"text": "DigHist",
+					"indices": [
+						232,
+						240
+					]
+				},
+				{
+					"text": "MuseTech",
+					"indices": [
+						241,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BL_DigiSchol",
+					"name": "Digital Scholarship",
+					"id": 744814089172455400,
+					"id_str": "744814089172455424",
+					"indices": [
+						18,
+						31
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/prmyOIwLff",
+					"expanded_url": "https://blogs.bl.uk/digital-scholarship/2020/07/readings-at-the-intersection-of-digital-scholarship-and-anti-racism.html",
+					"display_url": "blogs.bl.uk/digital-schola…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20431901,
+			"id_str": "20431901",
+			"name": "Dr Mia Ridge",
+			"screen_name": "mia_out",
+			"location": "London, mostly",
+			"description": "Digital Curator @BritishLibrary @BL_DigiSchol. Collections as data, UX, crowdsourcing in cultural heritage, libraries, museums. Digital History PhD. She, they.",
+			"url": "https://t.co/zRBZL6Mhqn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zRBZL6Mhqn",
+							"expanded_url": "http://www.miaridge.com/",
+							"display_url": "miaridge.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9073,
+			"friends_count": 493,
+			"listed_count": 721,
+			"created_at": "Mon Feb 09 12:32:56 +0000 2009",
+			"favourites_count": 6694,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 48834,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/666703610114416640/2471UIHT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/666703610114416640/2471UIHT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20431901/1475055023",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 38,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New blog post for @BL_DigiSchol, 'Readings at the intersection of digital scholarship and anti-racism' looks back at topics we've discussed to understand the inequalities and biases that tools and data can add to #DigitalHumanities #DigHist #MuseTech work https://t.co/prmyOIwLff"
+	},
+	{
 		"created_at": "Mon Jul 06 19:09:42 +0000 2020",
 		"id": 1280217377834057700,
 		"id_str": "1280217377834057729",
