c67d8c3f327e03440fee145523ba964b13c9ef3a
ellenforsyth <ellenforsyth@gmail.com>
2020-08-02 17:15:03 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37305,
-	"created_at": "Sat Aug 01 07:15:03 +0000 2020",
+	"tweets": 37306,
+	"created_at": "Sun Aug 02 07:15:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 01 20:24:25 +0000 2020",
+		"id": 1289085132112978000,
+		"id_str": "1289658264863436801",
+		"full_text": "Trinity College Dublin has begun transforming its front lawns into wildflower meadows after public support for the bee-friendly project. 🐝 💐 🌱 https://t.co/OFrs0xzfFj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1288899565702262800,
+					"id_str": "1288899565702262784",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1288899565702262784/img/hXSgS89MA6JZxF4_.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1288899565702262784/img/hXSgS89MA6JZxF4_.jpg",
+					"url": "https://t.co/OFrs0xzfFj",
+					"display_url": "pic.twitter.com/OFrs0xzfFj",
+					"expanded_url": "https://twitter.com/rtenews/status/1289085132112977923/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1288899565702262800,
+					"id_str": "1288899565702262784",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1288899565702262784/img/hXSgS89MA6JZxF4_.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1288899565702262784/img/hXSgS89MA6JZxF4_.jpg",
+					"url": "https://t.co/OFrs0xzfFj",
+					"display_url": "pic.twitter.com/OFrs0xzfFj",
+					"expanded_url": "https://twitter.com/rtenews/status/1289085132112977923/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 132560,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1288899565702262784/vid/480x270/PHOZ47iMEeJn6UnF.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1288899565702262784/pl/dznEaPDHwL_O4M2O.m3u8?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1288899565702262784/vid/1280x720/NB34HLRsM0jsXZy3.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1288899565702262784/vid/640x360/dYdlaH65yHwm5aYM.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8973062,
+			"id_str": "8973062",
+			"name": "RTÉ News",
+			"screen_name": "rtenews",
+			"location": "Ireland",
+			"description": "RTÉ News and Current Affairs - Irish and International News",
+			"url": "http://t.co/NopTRTpZP2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/NopTRTpZP2",
+							"expanded_url": "http://rte.ie/news",
+							"display_url": "rte.ie/news",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 863845,
+			"friends_count": 309,
+			"listed_count": 3602,
+			"created_at": "Wed Sep 19 12:52:21 +0000 2007",
+			"favourites_count": 40,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 165202,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/535035010596630529/3SmYLBeN_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/535035010596630529/3SmYLBeN_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8973062/1588330137",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1687,
+		"favorite_count": 6090,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Trinity College Dublin has begun transforming its front lawns into wildflower meadows after public support for the bee-friendly project. 🐝 💐 🌱 https://t.co/OFrs0xzfFj"
+	},
+	{
 		"created_at": "Sat Aug 01 07:00:38 +0000 2020",
 		"id": 1289455985002651600,
 		"id_str": "1289455985002651649",
