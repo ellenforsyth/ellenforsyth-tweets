67db4dc92747946e99f2eebac24fd463049eea36
ellenforsyth <ellenforsyth@gmail.com>
2020-09-05 17:15:04 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37360,
-	"created_at": "Fri Sep 04 07:15:01 +0000 2020",
+	"tweets": 37361,
+	"created_at": "Sat Sep 05 07:15:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 05 00:24:21 +0000 2020",
+		"id": 1300922321448779800,
+		"id_str": "1302039832101679104",
+		"full_text": "In March, I contracted #Covid_19 in France after attending a Magic the Gathering tournament as a guest artist. It took a long time to get well. https://t.co/c05H2iGPwk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Covid_19",
+					"indices": [
+						23,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c05H2iGPwk",
+					"expanded_url": "https://globalhealthc3.org/heroes/liz-danforth/",
+					"display_url": "globalhealthc3.org/heroes/liz-dan…",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14816341,
+			"id_str": "14816341",
+			"name": "Liz Danforth",
+			"screen_name": "LizDanforth",
+			"location": "Arizona",
+			"description": "illustrator, ex-librarian, writer, gamer geek, gaming professional & perpetually curious (she/her but they/them works too)",
+			"url": "http://t.co/nqfDJmAObd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/nqfDJmAObd",
+							"expanded_url": "http://www.lizdanforth.com",
+							"display_url": "lizdanforth.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2312,
+			"friends_count": 1014,
+			"listed_count": 168,
+			"created_at": "Sun May 18 00:56:20 +0000 2008",
+			"favourites_count": 3630,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13652,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFA10A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123698134838927365/y8t2KeRd_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123698134838927365/y8t2KeRd_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14816341/1484001491",
+			"profile_link_color": "B82500",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 43,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In March, I contracted #Covid_19 in France after attending a Magic the Gathering tournament as a guest artist. It took a long time to get well. https://t.co/c05H2iGPwk"
+	},
+	{
 		"created_at": "Thu Sep 03 20:26:23 +0000 2020",
 		"id": 1301617558869610500,
 		"id_str": "1301617558869610496",
