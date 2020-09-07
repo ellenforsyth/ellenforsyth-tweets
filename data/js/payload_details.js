2792c4d5826b978e94b1a31e3d3e1705c261faaa
ellenforsyth <ellenforsyth@gmail.com>
2020-09-07 17:15:07 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37362,
-	"created_at": "Sun Sep 06 07:15:02 +0000 2020",
+	"tweets": 37363,
+	"created_at": "Mon Sep 07 07:15:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sun Sep 06 23:00:18 +0000 2020",
+		"id": 1302740270375731200,
+		"id_str": "1302743458113712128",
+		"full_text": "Today is #ThreatenedSpeciesDay, raising awareness of plants &amp; animals at risk of extinction. It's also the anniversary of the death in captivity of the last known thylacine, sometimes called Benjamin. Earlier this year new footage of Benjamin was revealed. https://t.co/yvouEVHdXt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			284
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ThreatenedSpeciesDay",
+					"indices": [
+						9,
+						30
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yvouEVHdXt",
+					"expanded_url": "https://www.abc.net.au/news/2020-05-19/tasmanian-tiger-thylacine-footage-released/12263548",
+					"display_url": "abc.net.au/news/2020-05-1…",
+					"indices": [
+						261,
+						284
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2293603104,
+			"id_str": "2293603104",
+			"name": "CSIRO Publishing",
+			"screen_name": "CSIROPublishing",
+			"location": "Melbourne, Australia",
+			"description": "It starts with science.\n\nPart of CSIRO, but editorially independent, we have a global reputation for quality. RTs not endorsements.",
+			"url": "https://t.co/GDD0w99p7L",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GDD0w99p7L",
+							"expanded_url": "http://www.publish.csiro.au",
+							"display_url": "publish.csiro.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5058,
+			"friends_count": 917,
+			"listed_count": 113,
+			"created_at": "Thu Jan 16 00:52:10 +0000 2014",
+			"favourites_count": 1282,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11894,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1186514318885416960/5BctXo6o_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1186514318885416960/5BctXo6o_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2293603104/1569811904",
+			"profile_link_color": "2D5584",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 48,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is #ThreatenedSpeciesDay, raising awareness of plants &amp; animals at risk of extinction. It's also the anniversary of the death in captivity of the last known thylacine, sometimes called Benjamin. Earlier this year new footage of Benjamin was revealed. https://t.co/yvouEVHdXt"
+	},
+	{
 		"created_at": "Sat Sep 05 10:58:29 +0000 2020",
 		"id": 1302199415524843500,
 		"id_str": "1302199415524843520",
