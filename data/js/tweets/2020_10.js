5e54a910320fdd2a3984ac00201a69d9c0e35674
ellenforsyth <ellenforsyth@gmail.com>
2020-10-02 17:15:03 +1000

    Added 1 tweets

---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,117 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Thu Oct 01 20:23:37 +0000 2020",
+		"id": 1311578795036606500,
+		"id_str": "1311763724303179776",
+		"full_text": "‘Using your eyes as a pen’ – Black British Poets in Performance - Sound and vision blog https://t.co/PMVAqimTUQ #nationalpoetryday2020",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "nationalpoetryday2020",
+					"indices": [
+						112,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PMVAqimTUQ",
+					"expanded_url": "https://shar.es/abDepV",
+					"display_url": "shar.es/abDepV",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 26213705,
+			"id_str": "26213705",
+			"name": "BL Drama & Lit Sound",
+			"screen_name": "BL_DramaSound",
+			"location": "London",
+			"description": "AV recordings of poetry, prose, drama, live art, and sound made by artists: 100 years of commercial, broadcast and unique unpublished material.Tweets @EvadelRey",
+			"url": "http://t.co/TuSRYUV7Ns",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/TuSRYUV7Ns",
+							"expanded_url": "http://britishlibrary.typepad.co.uk/sound-and-vision",
+							"display_url": "britishlibrary.typepad.co.uk/sound-and-visi…",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2449,
+			"friends_count": 1281,
+			"listed_count": 86,
+			"created_at": "Tue Mar 24 10:52:54 +0000 2009",
+			"favourites_count": 674,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3247,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1285385416/blake_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1285385416/blake_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/26213705/1421780078",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "‘Using your eyes as a pen’ – Black British Poets in Performance - Sound and vision blog https://t.co/PMVAqimTUQ #nationalpoetryday2020"
+	}
+]
