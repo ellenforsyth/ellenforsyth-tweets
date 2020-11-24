fb82cc8163b262df4f4b00544d4d45b227dc68a8
ellenforsyth <ellenforsyth@gmail.com>
2020-11-24 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37455,
-	"created_at": "Mon Nov 23 07:15:01 +0000 2020",
+	"tweets": 37456,
+	"created_at": "Tue Nov 24 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Tue Nov 24 07:07:38 +0000 2020",
+		"id": 1331132350512566300,
+		"id_str": "1331132350512566272",
+		"full_text": "still time to join this session https://t.co/pJBIhkwiXa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pJBIhkwiXa",
+					"expanded_url": "https://twitter.com/rgibli/status/1325923305610641408",
+					"display_url": "twitter.com/rgibli/status/…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2543,
+			"friends_count": 878,
+			"listed_count": 228,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 6858,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37139,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1325923305610641400,
+		"quoted_status_id_str": "1325923305610641408",
+		"quoted_status_permalink": {
+			"url": "https://t.co/pJBIhkwiXa",
+			"expanded": "https://twitter.com/rgibli/status/1325923305610641408",
+			"display": "twitter.com/rgibli/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 09 22:08:45 +0000 2020",
+			"id": 1325923305610641400,
+			"id_str": "1325923305610641408",
+			"full_text": "Attention booklovers! Join us for the launch of Untapped: the Australian Literary Heritage Project. Nov 24, 6pm AEDT. \n\nhttps://t.co/XAIqLfj5te\n\nWe're rescuing lost literary treasures, getting authors paid, and supporting arts workers affected by COVID.\n\nPls help spread the word!",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/XAIqLfj5te",
+						"expanded_url": "https://events.unimelb.edu.au/MLS/event/9102-rescuing-australias-lost-literary-treasures",
+						"display_url": "events.unimelb.edu.au/MLS/event/9102…",
+						"indices": [
+							120,
+							143
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 256823402,
+				"id_str": "256823402",
+				"name": "Rebecca Giblin",
+				"screen_name": "rgibli",
+				"location": "Melbourne (Wurundjeri lands)",
+				"description": "ARC Future Fellow @melblawschool; Director of IPRIA. Authors' rights; A2K; tech reg. Genuinely listening. Everyone's just doing their best.",
+				"url": "https://t.co/7F0m9v0Dhl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7F0m9v0Dhl",
+								"expanded_url": "http://authorsinterest.org",
+								"display_url": "authorsinterest.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3091,
+				"friends_count": 841,
+				"listed_count": 111,
+				"created_at": "Thu Feb 24 04:14:17 +0000 2011",
+				"favourites_count": 630,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 923,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "A92731",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/958457010575499264/lgO0V2-I_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/958457010575499264/lgO0V2-I_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/256823402/1538047138",
+				"profile_link_color": "83AFA5",
+				"profile_sidebar_border_color": "DDE1BE",
+				"profile_sidebar_fill_color": "C7D2B5",
+				"profile_text_color": "7E6060",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 62,
+			"favorite_count": 55,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "still time to join this session https://t.co/pJBIhkwiXa"
+	},
+	{
 		"created_at": "Sun Nov 22 21:09:18 +0000 2020",
 		"id": 1330552507429392400,
 		"id_str": "1330619387871600641",
