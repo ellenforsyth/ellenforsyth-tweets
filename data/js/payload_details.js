2f731e1dd212b110b600ebde5e053f46a7e06df9
ellenforsyth <ellenforsyth@gmail.com>
2020-12-01 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37463,
-	"created_at": "Mon Nov 30 07:15:01 +0000 2020",
+	"tweets": 37464,
+	"created_at": "Tue Dec 01 07:15:01 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Tue Dec 01 06:44:14 +0000 2020",
+		"id": 1333661437894549500,
+		"id_str": "1333663178035580930",
+		"full_text": "Are you OK? https://t.co/MMbV2wm6va",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MMbV2wm6va",
+					"expanded_url": "https://ift.tt/3mrRAjU",
+					"display_url": "ift.tt/3mrRAjU",
+					"indices": [
+						12,
+						35
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 764365,
+			"id_str": "764365",
+			"name": "Doug Belshaw's writing feed",
+			"screen_name": "dajbelshaw",
+			"location": "The internet",
+			"description": "Auto-posting Doug's blog posts and articles from various places on the web.",
+			"url": "https://t.co/Y5ZxAMGZ5A",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y5ZxAMGZ5A",
+							"expanded_url": "https://dougbelshaw.com",
+							"display_url": "dougbelshaw.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14401,
+			"friends_count": 4568,
+			"listed_count": 1142,
+			"created_at": "Sun Feb 11 10:23:13 +0000 2007",
+			"favourites_count": 26143,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1740,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1321125991687856129/OO3cVUZ0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1321125991687856129/OO3cVUZ0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/764365/1603815958",
+			"profile_link_color": "2C3444",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Are you OK? https://t.co/MMbV2wm6va"
+	}
+]
