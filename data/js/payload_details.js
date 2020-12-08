8244c4fd7414189853831ab5f9a552cb45e220fa
ellenforsyth <ellenforsyth@gmail.com>
2020-12-08 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37472,
-	"created_at": "Mon Dec 07 07:15:01 +0000 2020",
+	"tweets": 37473,
+	"created_at": "Tue Dec 08 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 08 07:02:58 +0000 2020",
+		"id": 1336177364036776000,
+		"id_str": "1336204608620744704",
+		"full_text": "Iconic Test Pilot General Chuck Yeager has slipped the surly bonds of earth. A WWII fighter ace and the first human to break the sound barrier. \n\nHe was a true legend with the right stuff. Fair winds and following seas, General Yeager. #RIPChuckYeager https://t.co/1MxJA8enFU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			251
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RIPChuckYeager",
+					"indices": [
+						236,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1336177360488472600,
+					"id_str": "1336177360488472578",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EosOOuDVoAIKj5B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EosOOuDVoAIKj5B.jpg",
+					"url": "https://t.co/1MxJA8enFU",
+					"display_url": "pic.twitter.com/1MxJA8enFU",
+					"expanded_url": "https://twitter.com/StationCDRKelly/status/1336177364036775937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1336177360488472600,
+					"id_str": "1336177360488472578",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EosOOuDVoAIKj5B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EosOOuDVoAIKj5B.jpg",
+					"url": "https://t.co/1MxJA8enFU",
+					"display_url": "pic.twitter.com/1MxJA8enFU",
+					"expanded_url": "https://twitter.com/StationCDRKelly/status/1336177364036775937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 512,
+							"h": 490,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 65647594,
+			"id_str": "65647594",
+			"name": "Scott Kelly",
+			"screen_name": "StationCDRKelly",
+			"location": "Denver, CO",
+			"description": "Ret US Navy Capt & Former NASA Astronaut | #YearInSpace | @KepplerSpeakers | NY Times Bestselling Author |#EnduranceMemoir #InfiniteWonder #MyJourneyToTheStars",
+			"url": "https://t.co/MgvPwlxc4b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MgvPwlxc4b",
+							"expanded_url": "https://linktr.ee/StationCDRKelly",
+							"display_url": "linktr.ee/StationCDRKelly",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5455663,
+			"friends_count": 462,
+			"listed_count": 8180,
+			"created_at": "Fri Aug 14 14:31:39 +0000 2009",
+			"favourites_count": 1501,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3690,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "010505",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/975030326459170816/ivHlJOYx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/975030326459170816/ivHlJOYx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/65647594/1591778187",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "838582",
+			"profile_sidebar_fill_color": "262626",
+			"profile_text_color": "727273",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 272,
+		"favorite_count": 1502,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Iconic Test Pilot General Chuck Yeager has slipped the surly bonds of earth. A WWII fighter ace and the first human to break the sound barrier. \n\nHe was a true legend with the right stuff. Fair winds and following seas, General Yeager. #RIPChuckYeager https://t.co/1MxJA8enFU"
+	},
+	{
 		"created_at": "Sun Dec 06 23:19:09 +0000 2020",
 		"id": 1335725498144583700,
 		"id_str": "1335725498144583681",
