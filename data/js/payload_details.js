f64c49b8a12c93fa8366cad026d2810724c9e947
ellenforsyth <ellenforsyth@gmail.com>
2020-12-14 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37478,
-	"created_at": "Sat Dec 12 07:15:01 +0000 2020",
+	"tweets": 37479,
+	"created_at": "Mon Dec 14 07:15:01 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sun Dec 13 19:33:02 +0000 2020",
+		"id": 1338203121600716800,
+		"id_str": "1338205305969102849",
+		"full_text": "These are the plastic items that most kill whales, dolphins, turtles and seabirds https://t.co/kdT9oMUpaj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kdT9oMUpaj",
+					"expanded_url": "https://theconversation.com/these-are-the-plastic-items-that-most-kill-whales-dolphins-turtles-and-seabirds-151200",
+					"display_url": "theconversation.com/these-are-the-…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 894007020411473900,
+			"id_str": "894007020411473920",
+			"name": "The Pacific Newsroom",
+			"screen_name": "newsroom_the",
+			"location": "",
+			"description": "Aggregated independent news about the Pacific. Created by Ex Editor ABC Int. Sue Ahearn. Facebook. https://t.co/EaL5bgazXM",
+			"url": "https://t.co/9NOVKz0x9K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9NOVKz0x9K",
+							"expanded_url": "https://www.facebook.com/groups/137895163463995/",
+							"display_url": "facebook.com/groups/1378951…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/EaL5bgazXM",
+							"expanded_url": "https://www.facebook.com/groups/Pacificnewsroom/?ref=share",
+							"display_url": "facebook.com/groups/Pacific…",
+							"indices": [
+								99,
+								122
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2556,
+			"friends_count": 1697,
+			"listed_count": 37,
+			"created_at": "Sun Aug 06 01:27:38 +0000 2017",
+			"favourites_count": 4391,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/894009241647828993/1NWAvrQh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/894009241647828993/1NWAvrQh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/894007020411473920/1501983276",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "These are the plastic items that most kill whales, dolphins, turtles and seabirds https://t.co/kdT9oMUpaj"
+	},
+	{
 		"created_at": "Fri Dec 11 21:43:25 +0000 2020",
 		"id": 1337513341766443000,
 		"id_str": "1337513341766443010",
