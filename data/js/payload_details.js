ca1083b54d6a239aed50c2b499f914b6e12c04ec
ellenforsyth <ellenforsyth@gmail.com>
2021-01-07 18:15:02 +1100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37515,
-	"created_at": "Wed Jan 06 07:15:07 +0000 2021",
+	"tweets": 37517,
+	"created_at": "Thu Jan 07 07:15:01 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,451 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 06 22:46:54 +0000 2021",
+		"id": 1346950423484137500,
+		"id_str": "1346951403198038016",
+		"full_text": "Do you have a piece of history at home?\n\nWe welcome donations of materials relating to Aboriginal and Torres Strait Islander studies, culture, knowledge, and experience. \n\nGet in touch if you would like to donate material to AIATSIS. https://t.co/myCouAflOd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/myCouAflOd",
+					"expanded_url": "https://bit.ly/3m96bAP",
+					"display_url": "bit.ly/3m96bAP",
+					"indices": [
+						234,
+						257
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1523973469,
+			"id_str": "1523973469",
+			"name": "AIATSIS",
+			"screen_name": "AIATSIS",
+			"location": "Canberra",
+			"description": "We are Australia’s only national institution focused exclusively on the diverse history, cultures and heritage of First Nation Australians.",
+			"url": "https://t.co/cuFViFU3y0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cuFViFU3y0",
+							"expanded_url": "http://www.aiatsis.gov.au",
+							"display_url": "aiatsis.gov.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10141,
+			"friends_count": 1781,
+			"listed_count": 123,
+			"created_at": "Mon Jun 17 05:00:39 +0000 2013",
+			"favourites_count": 2796,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4620,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1308935133676748801/BxNSuwvD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1308935133676748801/BxNSuwvD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1523973469/1609734383",
+			"profile_link_color": "D64527",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Do you have a piece of history at home?\n\nWe welcome donations of materials relating to Aboriginal and Torres Strait Islander studies, culture, knowledge, and experience. \n\nGet in touch if you would like to donate material to AIATSIS. https://t.co/myCouAflOd"
+	},
+	{
+		"created_at": "Wed Jan 06 20:47:34 +0000 2021",
+		"id": 1346921371364823000,
+		"id_str": "1346921371364823044",
+		"full_text": "a bicycle themed reading group sound interesting https://t.co/rGO91KT88n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rGO91KT88n",
+					"expanded_url": "https://twitter.com/sanjoselibrary/status/1346547017531547649",
+					"display_url": "twitter.com/sanjoselibrary…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2540,
+			"friends_count": 884,
+			"listed_count": 224,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 7049,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37219,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1541190131",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1346547017531547600,
+		"quoted_status_id_str": "1346547017531547649",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rGO91KT88n",
+			"expanded": "https://twitter.com/sanjoselibrary/status/1346547017531547649",
+			"display": "twitter.com/sanjoselibrary…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 05 20:00:01 +0000 2021",
+			"id": 1346547017531547600,
+			"id_str": "1346547017531547649",
+			"full_text": "#SJPL and @bikesv have teamed up to launch the Women Bike Book Club!  For January we’re reading The Adventures of a Girl Called Bicycle by @ChristinaUss. \n\nRegister at https://t.co/TxNbnNznTJ\nVirtual Meeting: Wed., Jan. 27 from 6-7PM\n#RightToLibrary #BookClub https://t.co/SmKUbFVM4q",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				259
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SJPL",
+						"indices": [
+							0,
+							5
+						]
+					},
+					{
+						"text": "RightToLibrary",
+						"indices": [
+							234,
+							249
+						]
+					},
+					{
+						"text": "BookClub",
+						"indices": [
+							250,
+							259
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "bikesv",
+						"name": "SV Bike Coalition",
+						"id": 18275024,
+						"id_str": "18275024",
+						"indices": [
+							10,
+							17
+						]
+					},
+					{
+						"screen_name": "ChristinaUss",
+						"name": "Christina Uss",
+						"id": 3207565383,
+						"id_str": "3207565383",
+						"indices": [
+							139,
+							152
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/TxNbnNznTJ",
+						"expanded_url": "http://bit.ly/sjpl-BookClub",
+						"display_url": "bit.ly/sjpl-BookClub",
+						"indices": [
+							168,
+							191
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1346542186641281000,
+						"id_str": "1346542186641281025",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/Eq_g_AXU0AEk8_d.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eq_g_AXU0AEk8_d.jpg",
+						"url": "https://t.co/SmKUbFVM4q",
+						"display_url": "pic.twitter.com/SmKUbFVM4q",
+						"expanded_url": "https://twitter.com/sanjoselibrary/status/1346547017531547649/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1346542186641281000,
+						"id_str": "1346542186641281025",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/Eq_g_AXU0AEk8_d.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eq_g_AXU0AEk8_d.jpg",
+						"url": "https://t.co/SmKUbFVM4q",
+						"display_url": "pic.twitter.com/SmKUbFVM4q",
+						"expanded_url": "https://twitter.com/sanjoselibrary/status/1346547017531547649/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 82158644,
+				"id_str": "82158644",
+				"name": "San Jose Library",
+				"screen_name": "sanjoselibrary",
+				"location": "San Jose, California",
+				"description": "We are the San José Public Library & we’re your neighborhood connection to people & ideas—online & off! Proudly serving over 6 million visitors annually.",
+				"url": "https://t.co/r2R1knsgLD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/r2R1knsgLD",
+								"expanded_url": "http://www.sjpl.org",
+								"display_url": "sjpl.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4989,
+				"friends_count": 761,
+				"listed_count": 231,
+				"created_at": "Tue Oct 13 18:32:34 +0000 2009",
+				"favourites_count": 3276,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5516,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B2DFDA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1148398222542376960/zJdoV93t_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1148398222542376960/zJdoV93t_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/82158644/1606767113",
+				"profile_link_color": "02AF98",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "a bicycle themed reading group sound interesting https://t.co/rGO91KT88n"
+	},
+	{
 		"created_at": "Wed Jan 06 02:45:35 +0000 2021",
 		"id": 1346649081251131400,
 		"id_str": "1346649081251131392",
