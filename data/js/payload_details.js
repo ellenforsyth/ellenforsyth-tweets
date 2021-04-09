2cdf69d496483bac77ed091a7110d82d8fb60e61
ellenforsyth <ellenforsyth@gmail.com>
2021-04-09 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37602,
-	"created_at": "Thu Apr 01 07:15:00 +0000 2021",
+	"tweets": 37603,
+	"created_at": "Fri Apr 09 07:15:00 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Thu Apr 08 21:49:15 +0000 2021",
+		"id": 1380264451857768400,
+		"id_str": "1380276578614005760",
+		"full_text": "We’ve updated the Deaths Inside database.\n\nBy our count, there have been at least 474 Aboriginal deaths in custody since the Royal commission in 1991.  https://t.co/b798YKMYfR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/b798YKMYfR",
+					"expanded_url": "https://www.theguardian.com/australia-news/2021/apr/09/the-474-deaths-inside-rising-number-of-indigenous-deaths-in-custody-revealed?CMP=Share_iOSApp_Other",
+					"display_url": "theguardian.com/australia-news…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 34465477,
+			"id_str": "34465477",
+			"name": "Calla Wahlquist",
+			"screen_name": "callapilla",
+			"location": "Melbourne, Victoria",
+			"description": "Reporter @guardianaus. Co-creator #DeathsInside. Justice, animal welfare, looking outside the city. Pious yarn gardener. ✉️ DMs; calla.wahlquist@theguardian.com",
+			"url": "https://t.co/w5bBLlQzFW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w5bBLlQzFW",
+							"expanded_url": "http://www.theguardian.com/au",
+							"display_url": "theguardian.com/au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19135,
+			"friends_count": 2338,
+			"listed_count": 353,
+			"created_at": "Thu Apr 23 00:42:43 +0000 2009",
+			"favourites_count": 62314,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 28569,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "302C5B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1179268688098119681/eDlH-1Vs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1179268688098119681/eDlH-1Vs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/34465477/1541288172",
+			"profile_link_color": "3C6C84",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 63,
+		"favorite_count": 81,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We’ve updated the Deaths Inside database.\n\nBy our count, there have been at least 474 Aboriginal deaths in custody since the Royal commission in 1991.  https://t.co/b798YKMYfR"
+	}
+]
