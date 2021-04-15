80ce02d7615305390d10f1ce489f26e3af2f5efa
ellenforsyth <ellenforsyth@gmail.com>
2021-04-15 17:15:02 +1000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37603,
-	"created_at": "Fri Apr 09 07:15:00 +0000 2021",
+	"tweets": 37606,
+	"created_at": "Thu Apr 15 07:15:01 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,532 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Thu Apr 15 05:17:41 +0000 2021",
+		"id": 1382563757059051500,
+		"id_str": "1382563757059051521",
+		"full_text": "@CatyJ That sounds brilliant",
+		"truncated": false,
+		"display_text_range": [
+			7,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CatyJ",
+					"name": "CatyJ",
+					"id": 15732431,
+					"id_str": "15732431",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1378840946209480700,
+		"in_reply_to_status_id_str": "1378840946209480707",
+		"in_reply_to_user_id": 15732431,
+		"in_reply_to_user_id_str": "15732431",
+		"in_reply_to_screen_name": "CatyJ",
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2540,
+			"friends_count": 891,
+			"listed_count": 224,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 7392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37317,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1616529980",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@CatyJ That sounds brilliant"
+	},
+	{
+		"created_at": "Thu Apr 15 05:17:25 +0000 2021",
+		"id": 1381877009144455200,
+		"id_str": "1382563691539832832",
+		"full_text": "Behind the numbers are people. When someone dies in custody, their loved ones are left to carry an unimaginable burden. As we mark #RCIADIC30Years, we stand by the families who live with this pain. \n\nMore family statements here: https://t.co/Pv9Gn8bEuZ https://t.co/Ac9KoDexRg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RCIADIC30Years",
+					"indices": [
+						131,
+						146
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Pv9Gn8bEuZ",
+					"expanded_url": "https://www.natsils.org.au/wp-content/uploads/2020/12/NATSILS-Media-Release-Rolling-Stone-BLM.docx.pdf",
+					"display_url": "natsils.org.au/wp-content/upl…",
+					"indices": [
+						229,
+						252
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1381876532713414700,
+					"id_str": "1381876532713414656",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey1pYo_U4AAIYFl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey1pYo_U4AAIYFl.jpg",
+					"url": "https://t.co/Ac9KoDexRg",
+					"display_url": "pic.twitter.com/Ac9KoDexRg",
+					"expanded_url": "https://twitter.com/ALS_NSWACT/status/1381877009144455168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1381876532713414700,
+					"id_str": "1381876532713414656",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey1pYo_U4AAIYFl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey1pYo_U4AAIYFl.jpg",
+					"url": "https://t.co/Ac9KoDexRg",
+					"display_url": "pic.twitter.com/Ac9KoDexRg",
+					"expanded_url": "https://twitter.com/ALS_NSWACT/status/1381877009144455168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1381876532835012600,
+					"id_str": "1381876532835012615",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey1pYpcUUAc_uzm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey1pYpcUUAc_uzm.jpg",
+					"url": "https://t.co/Ac9KoDexRg",
+					"display_url": "pic.twitter.com/Ac9KoDexRg",
+					"expanded_url": "https://twitter.com/ALS_NSWACT/status/1381877009144455168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1381876610622660600,
+					"id_str": "1381876610622660608",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey1pdLOVoAA62qm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey1pdLOVoAA62qm.jpg",
+					"url": "https://t.co/Ac9KoDexRg",
+					"display_url": "pic.twitter.com/Ac9KoDexRg",
+					"expanded_url": "https://twitter.com/ALS_NSWACT/status/1381877009144455168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1381876611033653200,
+					"id_str": "1381876611033653248",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey1pdMwU4AA2KlD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey1pdMwU4AA2KlD.jpg",
+					"url": "https://t.co/Ac9KoDexRg",
+					"display_url": "pic.twitter.com/Ac9KoDexRg",
+					"expanded_url": "https://twitter.com/ALS_NSWACT/status/1381877009144455168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 902913672,
+			"id_str": "902913672",
+			"name": "Aboriginal Legal Service",
+			"screen_name": "ALS_NSWACT",
+			"location": "NSW/ACT",
+			"description": "Fighting for the rights of Aboriginal people in NSW & the ACT since 1970. We provide services in criminal law, care protection & family law, & tenancy matters.",
+			"url": "https://t.co/idpxZ6h9Yb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/idpxZ6h9Yb",
+							"expanded_url": "http://www.alsnswact.org.au",
+							"display_url": "alsnswact.org.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6524,
+			"friends_count": 2927,
+			"listed_count": 92,
+			"created_at": "Thu Oct 25 00:48:57 +0000 2012",
+			"favourites_count": 1475,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11902,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0A090A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2760761277/20665234df6d02a7455817ba350a13f2_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2760761277/20665234df6d02a7455817ba350a13f2_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/902913672/1591530292",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 180,
+		"favorite_count": 264,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Behind the numbers are people. When someone dies in custody, their loved ones are left to carry an unimaginable burden. As we mark #RCIADIC30Years, we stand by the families who live with this pain. \n\nMore family statements here: https://t.co/Pv9Gn8bEuZ https://t.co/Ac9KoDexRg"
+	},
+	{
+		"created_at": "Thu Apr 15 05:16:26 +0000 2021",
+		"id": 1382340813846364200,
+		"id_str": "1382563445753532416",
+		"full_text": "Today on the Blog: Using Awards Lists as a RA Tool: @TheHugoAwards Edition w/ an explanation of the difference between this and the Nebula. https://t.co/mJNr24ETpA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TheHugoAwards",
+					"name": "The Hugo Awards",
+					"id": 19443411,
+					"id_str": "19443411",
+					"indices": [
+						52,
+						66
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mJNr24ETpA",
+					"expanded_url": "http://raforall.blogspot.com/2021/04/using-awards-lists-as-ra-tool-hugo.html",
+					"display_url": "raforall.blogspot.com/2021/04/using-…",
+					"indices": [
+						140,
+						163
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2474412986,
+			"id_str": "2474412986",
+			"name": "Becky Spratford (She/Her)",
+			"screen_name": "RAforAll",
+			"location": "Chicagoland",
+			"description": "Matching books with readers thru the public library esp. #HorrorForLibraries. Jersey Girl in exile. @AmherstCollege '97, P '24 @HorrorWriters Secretary.",
+			"url": "https://t.co/UqsI4i1DEP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UqsI4i1DEP",
+							"expanded_url": "http://raforall.blogspot.com",
+							"display_url": "raforall.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3825,
+			"friends_count": 2729,
+			"listed_count": 146,
+			"created_at": "Fri May 02 18:51:55 +0000 2014",
+			"favourites_count": 29732,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 38950,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/555780672439070720/ltCP9rHK_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/555780672439070720/ltCP9rHK_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2474412986/1614280328",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today on the Blog: Using Awards Lists as a RA Tool: @TheHugoAwards Edition w/ an explanation of the difference between this and the Nebula. https://t.co/mJNr24ETpA"
+	},
+	{
 		"created_at": "Thu Apr 08 21:49:15 +0000 2021",
 		"id": 1380264451857768400,
 		"id_str": "1380276578614005760",
