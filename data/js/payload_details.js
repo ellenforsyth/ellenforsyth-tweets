395b80a39fc57f5a05aa3b5de970fbb460e7df58
ellenforsyth <ellenforsyth@gmail.com>
2021-06-06 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37630,
-	"created_at": "Fri Jun 04 07:14:59 +0000 2021",
+	"tweets": 37631,
+	"created_at": "Sun Jun 06 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 05 08:01:24 +0000 2021",
+		"id": 1400876318313652200,
+		"id_str": "1401086739666534406",
+		"full_text": "Resetting humanity’s relationship with nature will be the focus of #WorldEnvironmentDay, which also marks the launch of the @UN Decade on Ecosystem Restoration, a global push to prevent, halt and reverse ecosystem degradation. #GenerationRestoration https://t.co/5gZetiWQyF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WorldEnvironmentDay",
+					"indices": [
+						67,
+						87
+					]
+				},
+				{
+					"text": "GenerationRestoration",
+					"indices": [
+						227,
+						249
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UN",
+					"name": "United Nations",
+					"id": 14159148,
+					"id_str": "14159148",
+					"indices": [
+						124,
+						127
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5gZetiWQyF",
+					"expanded_url": "https://www.unep.org/news-and-stories/story/world-environment-day-2021-launches-global-drive-restore-nature",
+					"display_url": "unep.org/news-and-stori…",
+					"indices": [
+						250,
+						273
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 38146999,
+			"id_str": "38146999",
+			"name": "UN Environment Programme",
+			"screen_name": "UNEP",
+			"location": "Nairobi, Kenya",
+			"description": "Official account of the United Nations Environment Programme.  Join #GenerationRestoration this #WorldEnvironmentDay on 5 June",
+			"url": "https://t.co/IMhlOtS2BA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IMhlOtS2BA",
+							"expanded_url": "https://linktr.ee/UNEP",
+							"display_url": "linktr.ee/UNEP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1109614,
+			"friends_count": 2964,
+			"listed_count": 11101,
+			"created_at": "Wed May 06 09:34:32 +0000 2009",
+			"favourites_count": 12814,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 31675,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1186261881436229632/3NxNNjLZ_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1186261881436229632/3NxNNjLZ_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/38146999/1621260042",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1348,
+		"favorite_count": 2303,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Resetting humanity’s relationship with nature will be the focus of #WorldEnvironmentDay, which also marks the launch of the @UN Decade on Ecosystem Restoration, a global push to prevent, halt and reverse ecosystem degradation. #GenerationRestoration https://t.co/5gZetiWQyF"
+	},
+	{
 		"created_at": "Thu Jun 03 11:07:00 +0000 2021",
 		"id": 1400239819641344000,
 		"id_str": "1400408670345719812",
