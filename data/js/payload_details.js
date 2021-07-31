5f88dce7676998ce2614c08eac2fece466a7f160
ellenforsyth <ellenforsyth@gmail.com>
2021-07-31 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37694,
-	"created_at": "Fri Jul 30 07:15:00 +0000 2021",
+	"tweets": 37695,
+	"created_at": "Sat Jul 31 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,10 +1,128 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 30 21:19:53 +0000 2021",
+		"id": 1421219015997853700,
+		"id_str": "1421219015997853703",
+		"full_text": "Even the Sheet Cakes Are Bigger in Texas https://t.co/IcEr7SUY3T  via @nytimes food and local studies and the recipe looks interesting too",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nytimes",
+					"name": "The New York Times",
+					"id": 807095,
+					"id_str": "807095",
+					"indices": [
+						70,
+						78
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/IcEr7SUY3T",
+					"expanded_url": "https://www.nytimes.com/2021/07/23/dining/texas-sheet-cake-recipe.html?smid=tw-share",
+					"display_url": "nytimes.com/2021/07/23/din…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. Mostly on Gadigal or Gamilaraay country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2525,
+			"friends_count": 893,
+			"listed_count": 222,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 7703,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37395,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1622503416",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Even the Sheet Cakes Are Bigger in Texas https://t.co/IcEr7SUY3T  via @nytimes food and local studies and the recipe looks interesting too"
+	},
+	{
 		"created_at": "Thu Jul 29 19:28:32 +0000 2021",
 		"id": 1420828609795661800,
 		"id_str": "1420828609795661824",
-		"full_text": "From flowering weeds to feral cats and fire ants: These are the introduced pests costing Au… via ⁦@ABCscience⁩ this is disheartening reading https://t.co/NozwBiUTYt",
+		"full_text": "From flowering weeds to feral cats and fire ants: These are the introduced pests costing Au… via @ABCscience this is disheartening reading https://t.co/NozwBiUTYt",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -116,7 +234,7 @@ Grailbird.data.tweets_2021_07 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "From flowering weeds to feral cats and fire ants: These are the introduced pests costing Au… via ⁦@ABCscience⁩ this is disheartening reading https://t.co/NozwBiUTYt"
+		"text": "From flowering weeds to feral cats and fire ants: These are the introduced pests costing Au… via @ABCscience this is disheartening reading https://t.co/NozwBiUTYt"
 	},
 	{
 		"created_at": "Wed Jul 28 19:14:41 +0000 2021",
