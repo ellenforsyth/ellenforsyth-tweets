a9c551c5127838b305bf8b6b3e0cbc3db7b56cc9
ellenforsyth <ellenforsyth@gmail.com>
2021-09-02 17:15:02 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37754,
-	"created_at": "Wed Sep 01 07:15:00 +0000 2021",
+	"tweets": 37755,
+	"created_at": "Thu Sep 02 07:15:01 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_09.js
@@ -0,0 +1,251 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Wed Sep 01 23:43:24 +0000 2021",
+		"id": 1433213935889436700,
+		"id_str": "1433213935889436672",
+		"full_text": "I am inspired by the bunting https://t.co/NKdUYmteT1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NKdUYmteT1",
+					"expanded_url": "https://twitter.com/guardianaus/status/1433165541431250955",
+					"display_url": "twitter.com/guardianaus/st…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2532,
+			"friends_count": 897,
+			"listed_count": 223,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 7825,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1622503416",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1433165541431251000,
+		"quoted_status_id_str": "1433165541431250955",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NKdUYmteT1",
+			"expanded": "https://twitter.com/guardianaus/status/1433165541431250955",
+			"display": "twitter.com/guardianaus/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 01 20:31:06 +0000 2021",
+			"id": 1433165541431251000,
+			"id_str": "1433165541431250955",
+			"full_text": "Light in Victoria’s lockdown: ‘We’ve turned Hurstbridge yellow’ https://t.co/s37wwZakPV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				87
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/s37wwZakPV",
+						"expanded_url": "https://www.theguardian.com/lifeandstyle/gallery/2021/sep/02/light-in-victorias-lockdown-weve-turned-hurstbridge-yellow?utm_term=Autofeed&CMP=soc_568&utm_medium=Social&utm_source=Twitter#Echobox=1630519919",
+						"display_url": "theguardian.com/lifeandstyle/g…",
+						"indices": [
+							64,
+							87
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1092378031,
+				"id_str": "1092378031",
+				"name": "Guardian Australia",
+				"screen_name": "GuardianAus",
+				"location": "Australia",
+				"description": "Latest news from Guardian Australia\n\n📨 Newsletters: https://t.co/iUNmfKF1Ef\n📰 Contribute: https://t.co/BD5vlNOCo4\n👩‍💻 Live blog: https://t.co/fZdHED7j2x",
+				"url": "https://t.co/11mlSVzfGG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/11mlSVzfGG",
+								"expanded_url": "https://theguardian.com/au",
+								"display_url": "theguardian.com/au",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/iUNmfKF1Ef",
+								"expanded_url": "http://bit.ly/gdnausemails",
+								"display_url": "bit.ly/gdnausemails",
+								"indices": [
+									52,
+									75
+								]
+							},
+							{
+								"url": "https://t.co/BD5vlNOCo4",
+								"expanded_url": "http://bit.ly/gdnausupport",
+								"display_url": "bit.ly/gdnausupport",
+								"indices": [
+									90,
+									113
+								]
+							},
+							{
+								"url": "https://t.co/fZdHED7j2x",
+								"expanded_url": "http://bit.ly/covidliveblogs",
+								"display_url": "bit.ly/covidliveblogs",
+								"indices": [
+									129,
+									152
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 303668,
+				"friends_count": 164,
+				"listed_count": 2755,
+				"created_at": "Tue Jan 15 15:16:44 +0000 2013",
+				"favourites_count": 103,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 267811,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B2AFA9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1387646257879941121/5r1XRmJw_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387646257879941121/5r1XRmJw_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1092378031/1595225395",
+				"profile_link_color": "005789",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I am inspired by the bunting https://t.co/NKdUYmteT1"
+	}
+]
