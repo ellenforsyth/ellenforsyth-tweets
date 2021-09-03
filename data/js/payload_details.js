037ee39c85cdfd131cdb737ed8da75d0b52f1e28
ellenforsyth <ellenforsyth@gmail.com>
2021-09-03 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37755,
-	"created_at": "Thu Sep 02 07:15:01 +0000 2021",
+	"tweets": 37756,
+	"created_at": "Fri Sep 03 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 02 11:30:36 +0000 2021",
+		"id": 1433391907657318400,
+		"id_str": "1433391907657318401",
+		"full_text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via ⁦@washingtonpost⁩ this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "washingtonpost",
+					"name": "The Washington Post",
+					"id": 2467791,
+					"id_str": "2467791",
+					"indices": [
+						94,
+						109
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OkmkjZ0Hix",
+					"expanded_url": "https://www.washingtonpost.com/lifestyle/2021/08/31/chicken-traffic-patrol-school-hen/",
+					"display_url": "washingtonpost.com/lifestyle/2021…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal country.",
+			"url": "https://t.co/Tw5J1g7ySX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Tw5J1g7ySX",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2534,
+			"friends_count": 899,
+			"listed_count": 223,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 7829,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37470,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1622503416",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via ⁦@washingtonpost⁩ this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix"
+	},
+	{
 		"created_at": "Wed Sep 01 23:43:24 +0000 2021",
 		"id": 1433213935889436700,
 		"id_str": "1433213935889436672",
@@ -144,7 +262,7 @@ Grailbird.data.tweets_2021_09 =
 				"name": "Guardian Australia",
 				"screen_name": "GuardianAus",
 				"location": "Australia",
-				"description": "Latest news from Guardian Australia\n\n📨 Newsletters: https://t.co/iUNmfKF1Ef\n📰 Contribute: https://t.co/BD5vlNOCo4\n👩‍💻 Live blog: https://t.co/fZdHED7j2x",
+				"description": "Latest news from Guardian Australia\n\n📨 Newsletters: https://t.co/iUNmfKF1Ef\n📰 Contribute: https://t.co/BD5vlNOCo4\n👩💻 Live blog: https://t.co/fZdHED7j2x",
 				"url": "https://t.co/11mlSVzfGG",
 				"entities": {
 					"url": {
