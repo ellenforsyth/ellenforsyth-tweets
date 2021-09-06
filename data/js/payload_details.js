27bfdb79952be7019436f485acdf3ecb94305e97
ellenforsyth <ellenforsyth@gmail.com>
2021-09-06 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37756,
-	"created_at": "Fri Sep 03 07:15:00 +0000 2021",
+	"tweets": 37757,
+	"created_at": "Mon Sep 06 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,10 +1,117 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 05 10:36:29 +0000 2021",
+		"id": 1434354795364507600,
+		"id_str": "1434465451908599810",
+		"full_text": "Pleased as punch that we have published this from journo Emily Middleton at the Gilgandra Weekly. Vaccine queues and lockdown videos: how one little NSW town is dealing with Covid https://t.co/AbeVRwpTSe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AbeVRwpTSe",
+					"expanded_url": "https://www.theguardian.com/australia-news/2021/sep/05/vaccine-queues-and-lockdown-videos-how-one-little-nsw-town-is-dealing-with-covid",
+					"display_url": "theguardian.com/australia-news…",
+					"indices": [
+						180,
+						203
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 52012432,
+			"id_str": "52012432",
+			"name": "Gabrielle Chan",
+			"screen_name": "gabriellechan",
+			"location": "Wiradjuri country",
+			"description": "Writing, politics, rural life. Rusted Off (Penguin 2018). Why You Should Give A F*ck About Farming, Aug 31, 2021. Preorders available now via link.",
+			"url": "https://t.co/UDAdksUK3u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UDAdksUK3u",
+							"expanded_url": "https://linktr.ee/gabriellechanwrites",
+							"display_url": "linktr.ee/gabriellechanw…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16113,
+			"friends_count": 1956,
+			"listed_count": 300,
+			"created_at": "Mon Jun 29 09:44:59 +0000 2009",
+			"favourites_count": 4128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 25127,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BADFCD",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/987050403236741120/MMDI2Lys_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/987050403236741120/MMDI2Lys_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/52012432/1524166354",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "F2E195",
+			"profile_sidebar_fill_color": "FFF7CC",
+			"profile_text_color": "0C3E53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 27,
+		"favorite_count": 75,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Pleased as punch that we have published this from journo Emily Middleton at the Gilgandra Weekly. Vaccine queues and lockdown videos: how one little NSW town is dealing with Covid https://t.co/AbeVRwpTSe"
+	},
+	{
 		"created_at": "Thu Sep 02 11:30:36 +0000 2021",
 		"id": 1433391907657318400,
 		"id_str": "1433391907657318401",
-		"full_text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via ⁦@washingtonpost⁩ this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix",
+		"full_text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via @washingtonpost this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -116,7 +223,7 @@ Grailbird.data.tweets_2021_09 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via ⁦@washingtonpost⁩ this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix"
+		"text": "The traffic patrol officer at this school is a chicken. And it wears a fluorescent vest. Via @washingtonpost this is a lovely story from New Zealand  https://t.co/OkmkjZ0Hix"
 	},
 	{
 		"created_at": "Wed Sep 01 23:43:24 +0000 2021",
