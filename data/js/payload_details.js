05b25475014326467cd3aff1287331d6bef28929
ellenforsyth <ellenforsyth@gmail.com>
2021-09-14 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37773,
-	"created_at": "Sat Sep 11 07:15:02 +0000 2021",
+	"tweets": 37774,
+	"created_at": "Tue Sep 14 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Tue Sep 14 05:04:16 +0000 2021",
+		"id": 1437563000060358700,
+		"id_str": "1437643339671564288",
+		"full_text": "\"When I Was Struggling, #Libraries Gave Me a Place to Belong\" via @goodhousemag) https://t.co/muquDjvgQ4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Libraries",
+					"indices": [
+						24,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "goodhousemag",
+					"name": "Good Housekeeping",
+					"id": 32432308,
+					"id_str": "32432308",
+					"indices": [
+						66,
+						79
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/muquDjvgQ4",
+					"expanded_url": "http://ow.ly/KBNo50G9icC",
+					"display_url": "ow.ly/KBNo50G9icC",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15169290,
+			"id_str": "15169290",
+			"name": "Library Journal",
+			"screen_name": "LibraryJournal",
+			"location": "New York, NY",
+			"description": "Library views, news, and book reviews from LJ staffers",
+			"url": "http://t.co/WSOb61lYld",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/WSOb61lYld",
+							"expanded_url": "http://www.libraryjournal.com",
+							"display_url": "libraryjournal.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 241651,
+			"friends_count": 410,
+			"listed_count": 5836,
+			"created_at": "Thu Jun 19 13:51:48 +0000 2008",
+			"favourites_count": 3691,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 49694,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5E9F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1011300661726760969/aq50CI4f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1011300661726760969/aq50CI4f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15169290/1539277524",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "6C6C6C",
+			"profile_sidebar_fill_color": "CBCDC5",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"When I Was Struggling, #Libraries Gave Me a Place to Belong\" via @goodhousemag) https://t.co/muquDjvgQ4"
+	},
+	{
 		"created_at": "Fri Sep 10 23:02:52 +0000 2021",
 		"id": 1436465223183966200,
 		"id_str": "1436465223183966209",
@@ -501,7 +627,7 @@ Grailbird.data.tweets_2021_09 =
 			"name": "Solweig Mary",
 			"screen_name": "solweigmary",
 			"location": "Paris, France",
-			"description": "Social Media Strategist & Manager #Art #Science 🏛️🌷🌿\nCurrently: @Le_Museum, @FracCentre.\n#Museogeek #Musesocial #Scicomm #GenZ #Freelance\n\nShe/Her 🏳️‍🌈",
+			"description": "Social Media Strategist & Manager #Art #Science 🏛️🌷🌿\nCurrently: @Le_Museum, @FracCentre.\n#Museogeek #Musesocial #Scicomm #GenZ #Freelance\n\nShe/Her 🏳️🌈",
 			"url": "https://t.co/RHGhNuECVA",
 			"entities": {
 				"url": {
