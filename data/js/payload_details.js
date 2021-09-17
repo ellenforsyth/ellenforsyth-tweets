e3d174dfe1b320b9d0ba6902fac7fd4ba1c9a703
ellenforsyth <ellenforsyth@gmail.com>
2021-09-17 17:15:01 +1000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37778,
-	"created_at": "Thu Sep 16 07:15:01 +0000 2021",
+	"tweets": 37780,
+	"created_at": "Fri Sep 17 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,371 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 17 04:37:59 +0000 2021",
+		"id": 1438631281382674400,
+		"id_str": "1438723887080960005",
+		"full_text": "The California Room has a new blog series titled \"East Side Revelations\" featuring individuals &amp; community organizations with ties to the East Side. \n\n#SJPLBlogger, Estella, puts the spotlight on Sofia Mendoza, an #ESSJ legend. Read about Sofia's story at https://t.co/5563zCLjVm https://t.co/CiWPR9qwFw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SJPLBlogger",
+					"indices": [
+						155,
+						167
+					]
+				},
+				{
+					"text": "ESSJ",
+					"indices": [
+						218,
+						223
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5563zCLjVm",
+					"expanded_url": "http://sjpl.org/blog/east-side-revelations-sofia-mendoza",
+					"display_url": "sjpl.org/blog/east-side…",
+					"indices": [
+						260,
+						283
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1438288478282920000,
+					"id_str": "1438288478282919941",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E_XTwCiXMAUEKVL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_XTwCiXMAUEKVL.jpg",
+					"url": "https://t.co/CiWPR9qwFw",
+					"display_url": "pic.twitter.com/CiWPR9qwFw",
+					"expanded_url": "https://twitter.com/sanjoselibrary/status/1438631281382674434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438288478282920000,
+					"id_str": "1438288478282919941",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E_XTwCiXMAUEKVL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_XTwCiXMAUEKVL.jpg",
+					"url": "https://t.co/CiWPR9qwFw",
+					"display_url": "pic.twitter.com/CiWPR9qwFw",
+					"expanded_url": "https://twitter.com/sanjoselibrary/status/1438631281382674434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1438288478438101000,
+					"id_str": "1438288478438101004",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E_XTwDHXEAwF30Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_XTwDHXEAwF30Y.jpg",
+					"url": "https://t.co/CiWPR9qwFw",
+					"display_url": "pic.twitter.com/CiWPR9qwFw",
+					"expanded_url": "https://twitter.com/sanjoselibrary/status/1438631281382674434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1438288478727508000,
+					"id_str": "1438288478727507969",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E_XTwEMXEAEsk7D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_XTwEMXEAEsk7D.jpg",
+					"url": "https://t.co/CiWPR9qwFw",
+					"display_url": "pic.twitter.com/CiWPR9qwFw",
+					"expanded_url": "https://twitter.com/sanjoselibrary/status/1438631281382674434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 82158644,
+			"id_str": "82158644",
+			"name": "San Jose Library",
+			"screen_name": "sanjoselibrary",
+			"location": "San Jose, California",
+			"description": "Serving San Jose’s diverse community since 1880. We’re your #1 provider of access to free tech devices, learning materials, streaming services, and more!",
+			"url": "https://t.co/x3SkvQnuez",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x3SkvQnuez",
+							"expanded_url": "https://www.sjpl.org",
+							"display_url": "sjpl.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5078,
+			"friends_count": 761,
+			"listed_count": 228,
+			"created_at": "Tue Oct 13 18:32:34 +0000 2009",
+			"favourites_count": 3511,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6035,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1396863797235654664/fnb6SKvx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1396863797235654664/fnb6SKvx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/82158644/1629932921",
+			"profile_link_color": "02AF98",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The California Room has a new blog series titled \"East Side Revelations\" featuring individuals &amp; community organizations with ties to the East Side. \n\n#SJPLBlogger, Estella, puts the spotlight on Sofia Mendoza, an #ESSJ legend. Read about Sofia's story at https://t.co/5563zCLjVm https://t.co/CiWPR9qwFw"
+	},
+	{
+		"created_at": "Fri Sep 17 01:51:01 +0000 2021",
+		"id": 1438646324807680000,
+		"id_str": "1438681867142586368",
+		"full_text": "Beehive vandalism hits honey producers' hearts and hip pockets https://t.co/l9vkbxQSH7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/l9vkbxQSH7",
+					"expanded_url": "https://www.abc.net.au/news/2021-09-17/beehive-vandalism-saddens-honey-producers/100467432?utm_campaign=news-article-next-actions-panel-2-0&utm_content=twitter&utm_medium=content_shared&utm_source=abc_news_web",
+					"display_url": "abc.net.au/news/2021-09-1…",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1306256097737539600,
+			"id_str": "1306256097737539594",
+			"name": "Genevieve Poole",
+			"screen_name": "GenevievePool12",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 885,
+			"friends_count": 4999,
+			"listed_count": 2,
+			"created_at": "Wed Sep 16 15:39:01 +0000 2020",
+			"favourites_count": 40738,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 22476,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1306259215028158464/FiaKUwpm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1306259215028158464/FiaKUwpm_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Beehive vandalism hits honey producers' hearts and hip pockets https://t.co/l9vkbxQSH7"
+	},
+	{
 		"created_at": "Thu Sep 16 03:15:16 +0000 2021",
 		"id": 1438340683299844000,
 		"id_str": "1438340683299844102",
