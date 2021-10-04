284eca2329ced7bc49a6ed887a6335cb32d5e0c9
ellenforsyth <ellenforsyth@gmail.com>
2021-10-04 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37790,
-	"created_at": "Sat Oct 02 07:15:00 +0000 2021",
+	"tweets": 37791,
+	"created_at": "Mon Oct 04 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sun Oct 03 20:31:34 +0000 2021",
+		"id": 1444084121307861000,
+		"id_str": "1444762071766233088",
+		"full_text": "2021 Knitting History Forum conference: HEADS, HANDS &amp; FEET Sat 13 Nov online. More info on our website https://t.co/nuJOLlMIoJ or book tickets https://t.co/BX44lNvEP4 Attendance free, donations welcome #knittinghistory #knitting #textilehistory #khfconference2021 #khfconference",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "knittinghistory",
+					"indices": [
+						207,
+						223
+					]
+				},
+				{
+					"text": "knitting",
+					"indices": [
+						224,
+						233
+					]
+				},
+				{
+					"text": "textilehistory",
+					"indices": [
+						234,
+						249
+					]
+				},
+				{
+					"text": "khfconference2021",
+					"indices": [
+						250,
+						268
+					]
+				},
+				{
+					"text": "khfconference",
+					"indices": [
+						269,
+						283
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nuJOLlMIoJ",
+					"expanded_url": "https://knittinghistory.co.uk/events/knitting-history-forum-conference-2021-heads-hands-feet/",
+					"display_url": "knittinghistory.co.uk/events/knittin…",
+					"indices": [
+						108,
+						131
+					]
+				},
+				{
+					"url": "https://t.co/BX44lNvEP4",
+					"expanded_url": "https://www.eventbrite.co.uk/e/khf-online-conference-2021-heads-hands-feet-tickets-181878713127",
+					"display_url": "eventbrite.co.uk/e/khf-online-c…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1735233036,
+			"id_str": "1735233036",
+			"name": "Knitting History Forum",
+			"screen_name": "KnitHistForum",
+			"location": "",
+			"description": "Knitting History Forum, international society for the history of knitting and crochet.",
+			"url": "http://t.co/a06RcMB4mK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/a06RcMB4mK",
+							"expanded_url": "http://knittinghistory.co.uk",
+							"display_url": "knittinghistory.co.uk",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1453,
+			"friends_count": 91,
+			"listed_count": 26,
+			"created_at": "Fri Sep 06 14:20:08 +0000 2013",
+			"favourites_count": 219,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 460,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C8E6E6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/553334642883842048/LprOobqv_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/553334642883842048/LprOobqv_normal.jpeg",
+			"profile_link_color": "0F6A66",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 27,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "2021 Knitting History Forum conference: HEADS, HANDS &amp; FEET Sat 13 Nov online. More info on our website https://t.co/nuJOLlMIoJ or book tickets https://t.co/BX44lNvEP4 Attendance free, donations welcome #knittinghistory #knitting #textilehistory #khfconference2021 #khfconference"
+	},
+	{
 		"created_at": "Sat Oct 02 00:32:14 +0000 2021",
 		"id": 1444086237074280400,
 		"id_str": "1444097860136419330",
