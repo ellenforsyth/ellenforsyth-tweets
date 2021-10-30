6cf3f1a3e43dc8d01145b92b32cb614a1040de19
ellenforsyth <ellenforsyth@gmail.com>
2021-10-30 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37809,
-	"created_at": "Thu Oct 28 07:15:00 +0000 2021",
+	"tweets": 37810,
+	"created_at": "Sat Oct 30 07:15:00 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,131 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 29 18:37:26 +0000 2021",
+		"id": 1454051057718513700,
+		"id_str": "1454155429995450369",
+		"full_text": "New public library in #Singapore https://t.co/9lsJukH7sv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Singapore",
+					"indices": [
+						22,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9lsJukH7sv",
+					"expanded_url": "https://mothership.sg/2021/10/choa-chu-kang-library/?fbclid=IwAR10DXC4JtRqJSgvJqZzJD5oBV9Hm9aXgR1BNW1p6rF286mwXram9nYxfHo",
+					"display_url": "mothership.sg/2021/10/choa-c…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 719470883094585300,
+			"id_str": "719470883094585344",
+			"name": "IFLA Section - Library Buildings and Equipment",
+			"screen_name": "ifla_lbes",
+			"location": "",
+			"description": "Winner IFLA Dynamic Unit Award 2019 #iflabuild2021 #library #buildings and #equipment section of @ifla https://t.co/arzConj4dx",
+			"url": "https://t.co/ay2Ikew6QN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ay2Ikew6QN",
+							"expanded_url": "http://www.ifla.org/library-buildings-and-equipment",
+							"display_url": "ifla.org/library-buildi…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/arzConj4dx",
+							"expanded_url": "https://www.facebook.com/groups/1649951498620366/",
+							"display_url": "facebook.com/groups/1649951…",
+							"indices": [
+								103,
+								126
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 901,
+			"friends_count": 90,
+			"listed_count": 33,
+			"created_at": "Mon Apr 11 10:23:20 +0000 2016",
+			"favourites_count": 1408,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1942,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1073262729396408325/m_K34zH__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1073262729396408325/m_K34zH__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/719470883094585344/1633477105",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New public library in #Singapore https://t.co/9lsJukH7sv"
+	},
+	{
 		"created_at": "Wed Oct 27 09:53:31 +0000 2021",
 		"id": 1453298806812991500,
 		"id_str": "1453298806812991490",
@@ -144,7 +269,7 @@ Grailbird.data.tweets_2021_10 =
 				"name": "Guardian Australia",
 				"screen_name": "GuardianAus",
 				"location": "Australia",
-				"description": "Latest news from Guardian Australia\n\n📨 Newsletters: https://t.co/iUNmfKF1Ef\n📰 Contribute: https://t.co/BD5vlNOCo4\n👩‍💻 Live blog: https://t.co/fZdHED7j2x",
+				"description": "Latest news from Guardian Australia\n\n📨 Newsletters: https://t.co/iUNmfKF1Ef\n📰 Contribute: https://t.co/BD5vlNOCo4\n👩💻 Live blog: https://t.co/fZdHED7j2x",
 				"url": "https://t.co/11mlSVzfGG",
 				"entities": {
 					"url": {
