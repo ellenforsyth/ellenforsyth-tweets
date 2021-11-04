8988ad7a155762c7a31d4e12a1b082072c58f51a
ellenforsyth <ellenforsyth@gmail.com>
2021-11-04 18:15:01 +1100

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,258 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 03 07:33:44 +0000 2021",
+		"id": 1455139244318544000,
+		"id_str": "1455800343606882304",
+		"full_text": "Today’s #RecordingOfTheWeek is a jarija work chant, sung during ploughing at a maize sowing ceremony in Santiago de Chocorvos, Peru, 1971 🌽\n\nRead our latest @soundarchive blog:  https://t.co/eCaMKH69yd\n\nDigitised as part of #UnlockingOurSoundHeritage @BL_WorldTrad https://t.co/i2WIojIYGU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RecordingOfTheWeek",
+					"indices": [
+						8,
+						27
+					]
+				},
+				{
+					"text": "UnlockingOurSoundHeritage",
+					"indices": [
+						224,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "soundarchive",
+					"name": "Sound Archive",
+					"id": 22028935,
+					"id_str": "22028935",
+					"indices": [
+						157,
+						170
+					]
+				},
+				{
+					"screen_name": "BL_WorldTrad",
+					"name": "BL World&Trad Music",
+					"id": 269268552,
+					"id_str": "269268552",
+					"indices": [
+						251,
+						264
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/eCaMKH69yd",
+					"expanded_url": "https://blogs.bl.uk/sound-and-vision/2021/11/recording-of-the-week-preserving-the-peruvian-jarija.html",
+					"display_url": "blogs.bl.uk/sound-and-visi…",
+					"indices": [
+						178,
+						201
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1455139211665887200,
+					"id_str": "1455139211665887237",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1455139211665887237/pu/img/8JAzAo9qlPIuB6q1.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1455139211665887237/pu/img/8JAzAo9qlPIuB6q1.jpg",
+					"url": "https://t.co/i2WIojIYGU",
+					"display_url": "pic.twitter.com/i2WIojIYGU",
+					"expanded_url": "https://twitter.com/BLSoundHeritage/status/1455139244318543880/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1455139211665887200,
+					"id_str": "1455139211665887237",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1455139211665887237/pu/img/8JAzAo9qlPIuB6q1.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1455139211665887237/pu/img/8JAzAo9qlPIuB6q1.jpg",
+					"url": "https://t.co/i2WIojIYGU",
+					"display_url": "pic.twitter.com/i2WIojIYGU",
+					"expanded_url": "https://twitter.com/BLSoundHeritage/status/1455139244318543880/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 47132,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1455139211665887237/pu/vid/540x540/ZQfyk7BmXAXMK-dk.mp4?tag=12"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1455139211665887237/pu/vid/320x320/PfDG0lz2ycDwWu_m.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1455139211665887237/pu/vid/720x720/q3M96xF5d8w-Ryo5.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1455139211665887237/pu/pl/1IXFPntNp4AAn0jh.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2828116855,
+			"id_str": "2828116855",
+			"name": "BL Sound Heritage",
+			"screen_name": "BLSoundHeritage",
+			"location": "British Library, London",
+			"description": "Sharing sounds and keeping them safe for generations to come #SaveOurSounds Tweets by the Unlocking Our Sound Heritage team",
+			"url": "http://t.co/iYnzfTEp8J",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/iYnzfTEp8J",
+							"expanded_url": "http://britishlibrary.typepad.co.uk/sound-and-vision/",
+							"display_url": "britishlibrary.typepad.co.uk/sound-and-visi…",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4524,
+			"friends_count": 1412,
+			"listed_count": 96,
+			"created_at": "Tue Sep 23 13:44:14 +0000 2014",
+			"favourites_count": 6745,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/514433033454096385/nKb7eCRA_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/514433033454096385/nKb7eCRA_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2828116855/1556706938",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today’s #RecordingOfTheWeek is a jarija work chant, sung during ploughing at a maize sowing ceremony in Santiago de Chocorvos, Peru, 1971 🌽\n\nRead our latest @soundarchive blog:  https://t.co/eCaMKH69yd\n\nDigitised as part of #UnlockingOurSoundHeritage @BL_WorldTrad https://t.co/i2WIojIYGU"
+	},
+	{
 		"created_at": "Tue Nov 02 18:14:59 +0000 2021",
 		"id": 1455381183244554200,
 		"id_str": "1455599334116839431",
