1ff12b9a660074d745a2d49ed04136f6d2dcfc5d
ellenforsyth <ellenforsyth@gmail.com>
2022-02-05 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37889,
-	"created_at": "Thu Feb 03 07:15:00 +0000 2022",
+	"tweets": 37890,
+	"created_at": "Sat Feb 05 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,285 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 04 21:37:31 +0000 2022",
+		"id": 1489714762162212900,
+		"id_str": "1489714762162212872",
+		"full_text": "make sure you read the article by Josh Chan, which also has some really useful references to explore https://t.co/QspKq4BmGM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QspKq4BmGM",
+					"expanded_url": "https://twitter.com/artefactors/status/1488478485828018176",
+					"display_url": "twitter.com/artefactors/st…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2531,
+			"friends_count": 931,
+			"listed_count": 0,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 8288,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37565,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1622503416",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1488478485828018200,
+		"quoted_status_id_str": "1488478485828018176",
+		"quoted_status_permalink": {
+			"url": "https://t.co/QspKq4BmGM",
+			"expanded": "https://twitter.com/artefactors/status/1488478485828018176",
+			"display": "twitter.com/artefactors/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 01 11:45:00 +0000 2022",
+			"id": 1488478485828018200,
+			"id_str": "1488478485828018176",
+			"full_text": "Newslet- our fortnightly newsletter for library and information staff- latest issue here:\nhttps://t.co/1VkZk4d5du #libraryrecruitment #OpenAccess #Wellbeing #Indigenouslibrarianship #OAadvocacy #academicpublishing #EquityDiversityInclusion \nSign up here https://t.co/W4859lO7T8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				277
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "libraryrecruitment",
+						"indices": [
+							114,
+							133
+						]
+					},
+					{
+						"text": "OpenAccess",
+						"indices": [
+							134,
+							145
+						]
+					},
+					{
+						"text": "Wellbeing",
+						"indices": [
+							146,
+							156
+						]
+					},
+					{
+						"text": "Indigenouslibrarianship",
+						"indices": [
+							157,
+							181
+						]
+					},
+					{
+						"text": "OAadvocacy",
+						"indices": [
+							182,
+							193
+						]
+					},
+					{
+						"text": "academicpublishing",
+						"indices": [
+							194,
+							213
+						]
+					},
+					{
+						"text": "EquityDiversityInclusion",
+						"indices": [
+							214,
+							239
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/1VkZk4d5du",
+						"expanded_url": "https://mailer.artefacto.org.uk/y0w6g4v4q9",
+						"display_url": "mailer.artefacto.org.uk/y0w6g4v4q9",
+						"indices": [
+							90,
+							113
+						]
+					},
+					{
+						"url": "https://t.co/W4859lO7T8",
+						"expanded_url": "https://libraries.newslet.org/",
+						"display_url": "libraries.newslet.org",
+						"indices": [
+							254,
+							277
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 880391678,
+				"id_str": "880391678",
+				"name": "artefacto",
+				"screen_name": "artefactors",
+				"location": "London",
+				"description": "Librarians & Makers. London-based creative tech company. We work with cultural organisations to bring innovative ideas to life",
+				"url": "https://t.co/PetU7lX8ih",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PetU7lX8ih",
+								"expanded_url": "https://www.artefacto.org.uk",
+								"display_url": "artefacto.org.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 945,
+				"friends_count": 746,
+				"listed_count": 46,
+				"created_at": "Sun Oct 14 15:41:42 +0000 2012",
+				"favourites_count": 2245,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4473,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/948154479739891713/NXRsxQux_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/948154479739891713/NXRsxQux_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/880391678/1389810660",
+				"profile_link_color": "4A5583",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "make sure you read the article by Josh Chan, which also has some really useful references to explore https://t.co/QspKq4BmGM"
+	},
+	{
 		"created_at": "Wed Feb 02 18:24:07 +0000 2022",
 		"id": 1488941314854908000,
 		"id_str": "1488941314854907905",
