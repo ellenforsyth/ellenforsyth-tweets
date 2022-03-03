6a777452558e8b6bbf9fda39156d461a2ed12325
ellenforsyth <ellenforsyth@gmail.com>
2022-03-03 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37908,
-	"created_at": "Tue Mar 01 07:15:00 +0000 2022",
+	"tweets": 37909,
+	"created_at": "Thu Mar 03 07:15:00 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,291 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Wed Mar 02 20:04:10 +0000 2022",
+		"id": 1499113354840842200,
+		"id_str": "1499113354840842241",
+		"full_text": "interesting project https://t.co/264SManzxM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/264SManzxM",
+					"expanded_url": "https://twitter.com/anlanntair/status/1496084779837669381",
+					"display_url": "twitter.com/anlanntair/sta…",
+					"indices": [
+						20,
+						43
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2532,
+			"friends_count": 936,
+			"listed_count": 218,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 8376,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37585,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1496084779837669400,
+		"quoted_status_id_str": "1496084779837669381",
+		"quoted_status_permalink": {
+			"url": "https://t.co/264SManzxM",
+			"expanded": "https://twitter.com/anlanntair/status/1496084779837669381",
+			"display": "twitter.com/anlanntair/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 22 11:29:42 +0000 2022",
+			"id": 1496084779837669400,
+			"id_str": "1496084779837669381",
+			"full_text": "Time to sign up for our last two Storm Stories Sharing events! \n\nWe would love to hear your stories and experiences of winter storms and stormy weather. Tell us your observations, thoughts and how your local environment might be changing.\n\n🔗 below https://t.co/a8QR1Oh0Y0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				247
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1496082751564484600,
+						"id_str": "1496082751564484611",
+						"indices": [
+							248,
+							271
+						],
+						"media_url": "http://pbs.twimg.com/media/FMMnVgGXEAMOMpY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FMMnVgGXEAMOMpY.jpg",
+						"url": "https://t.co/a8QR1Oh0Y0",
+						"display_url": "pic.twitter.com/a8QR1Oh0Y0",
+						"expanded_url": "https://twitter.com/anlanntair/status/1496084779837669381/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 595,
+								"h": 841,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 481,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 595,
+								"h": 841,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1496082751564484600,
+						"id_str": "1496082751564484611",
+						"indices": [
+							248,
+							271
+						],
+						"media_url": "http://pbs.twimg.com/media/FMMnVgGXEAMOMpY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FMMnVgGXEAMOMpY.jpg",
+						"url": "https://t.co/a8QR1Oh0Y0",
+						"display_url": "pic.twitter.com/a8QR1Oh0Y0",
+						"expanded_url": "https://twitter.com/anlanntair/status/1496084779837669381/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 595,
+								"h": 841,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 481,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 595,
+								"h": 841,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 285036750,
+				"id_str": "285036750",
+				"name": "An Lanntair",
+				"screen_name": "anlanntair",
+				"location": "Stornoway, Isle of Lewis",
+				"description": "An Lanntair - bringing you the best in Arts, Events, Music and Entertainment in the Western Isles.",
+				"url": "https://t.co/qvJpmLCu0Y",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qvJpmLCu0Y",
+								"expanded_url": "https://lanntair.com/",
+								"display_url": "lanntair.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6646,
+				"friends_count": 2235,
+				"listed_count": 98,
+				"created_at": "Wed Apr 20 12:24:59 +0000 2011",
+				"favourites_count": 5406,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5879,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1465325151504015363/9MWJvwD9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1465325151504015363/9MWJvwD9_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/285036750/1644838917",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "interesting project https://t.co/264SManzxM"
+	}
+]
