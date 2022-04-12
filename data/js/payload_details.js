d2b7694546b4c6a5cc533837ad281495f5bbd17c
ellenforsyth <ellenforsyth@gmail.com>
2022-04-12 17:15:00 +1000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37926,
-	"created_at": "Mon Apr 11 07:15:00 +0000 2022",
+	"tweets": 37928,
+	"created_at": "Tue Apr 12 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,399 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 12 00:08:16 +0000 2022",
+		"id": 1513666489189994500,
+		"id_str": "1513670296837844999",
+		"full_text": "Our friends in the @ANULibrary Digital Scholarship team have been digitising thousands of photos from our Waterside Workers' Federation and Maritime Union of Australia archives. You can see more on our FB page and Open Research website https://t.co/2fpe22rSNZ @MaritimeUnionAU https://t.co/xMVkt4KbTm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ANULibrary",
+					"name": "ANU Library",
+					"id": 706612250,
+					"id_str": "706612250",
+					"indices": [
+						19,
+						30
+					]
+				},
+				{
+					"screen_name": "MaritimeUnionAU",
+					"name": "MUA",
+					"id": 90555260,
+					"id_str": "90555260",
+					"indices": [
+						260,
+						276
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2fpe22rSNZ",
+					"expanded_url": "https://openresearch-repository.anu.edu.au/handle/1885/258260",
+					"display_url": "openresearch-repository.anu.edu.au/handle/1885/25…",
+					"indices": [
+						236,
+						259
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1513666023471390700,
+					"id_str": "1513666023471390724",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FQGfOoWXIAQ74_I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQGfOoWXIAQ74_I.jpg",
+					"url": "https://t.co/xMVkt4KbTm",
+					"display_url": "pic.twitter.com/xMVkt4KbTm",
+					"expanded_url": "https://twitter.com/TheANUArchives/status/1513666489189994496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1498,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 497,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 877,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1513666023471390700,
+					"id_str": "1513666023471390724",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FQGfOoWXIAQ74_I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQGfOoWXIAQ74_I.jpg",
+					"url": "https://t.co/xMVkt4KbTm",
+					"display_url": "pic.twitter.com/xMVkt4KbTm",
+					"expanded_url": "https://twitter.com/TheANUArchives/status/1513666489189994496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1498,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 497,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 877,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1513666145999544300,
+					"id_str": "1513666145999544320",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FQGfVwzWYAASiPZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQGfVwzWYAASiPZ.jpg",
+					"url": "https://t.co/xMVkt4KbTm",
+					"display_url": "pic.twitter.com/xMVkt4KbTm",
+					"expanded_url": "https://twitter.com/TheANUArchives/status/1513666489189994496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 544,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1638,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1513666335292948500,
+					"id_str": "1513666335292948480",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FQGfgx-agAAtSFc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQGfgx-agAAtSFc.jpg",
+					"url": "https://t.co/xMVkt4KbTm",
+					"display_url": "pic.twitter.com/xMVkt4KbTm",
+					"expanded_url": "https://twitter.com/TheANUArchives/status/1513666489189994496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1451,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 850,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 482,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2329690088,
+			"id_str": "2329690088",
+			"name": "The ANU Archives",
+			"screen_name": "TheANUArchives",
+			"location": "the ANU, Canberra, Australia",
+			"description": "We preserve the ANU archives, the National AIDS Archive, and archives of business, trade unions, professions and industry in the Noel Butlin Archives Centre.",
+			"url": "http://t.co/tTry0SFdcE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tTry0SFdcE",
+							"expanded_url": "http://archives.anu.edu.au/",
+							"display_url": "archives.anu.edu.au",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 739,
+			"friends_count": 210,
+			"listed_count": 14,
+			"created_at": "Thu Feb 06 03:10:29 +0000 2014",
+			"favourites_count": 1507,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 607,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/431297674302595073/nBaULY0f_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/431297674302595073/nBaULY0f_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2329690088/1391664372",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 11,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our friends in the @ANULibrary Digital Scholarship team have been digitising thousands of photos from our Waterside Workers' Federation and Maritime Union of Australia archives. You can see more on our FB page and Open Research website https://t.co/2fpe22rSNZ @MaritimeUnionAU https://t.co/xMVkt4KbTm"
+	},
+	{
+		"created_at": "Mon Apr 11 21:49:36 +0000 2022",
+		"id": 1513635402753908700,
+		"id_str": "1513635402753908736",
+		"full_text": "Brazilian BookTubers and the COVID-19 pandemic https://t.co/mb6iBNYGng via #FirstMonday interesting look at reading",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FirstMonday",
+					"indices": [
+						75,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mb6iBNYGng",
+					"expanded_url": "https://firstmonday.org/ojs/index.php/fm/article/view/12579",
+					"display_url": "firstmonday.org/ojs/index.php/…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2541,
+			"friends_count": 945,
+			"listed_count": 218,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 8595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37608,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Brazilian BookTubers and the COVID-19 pandemic https://t.co/mb6iBNYGng via #FirstMonday interesting look at reading"
+	},
+	{
 		"created_at": "Sun Apr 10 20:30:47 +0000 2022",
 		"id": 1513253178560893000,
 		"id_str": "1513253178560892929",
