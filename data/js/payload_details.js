30b2006461ac324c57db646e09e3155d2a2a333f
ellenforsyth <ellenforsyth@gmail.com>
2022-06-19 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37951,
-	"created_at": "Sat Jun 18 07:15:00 +0000 2022",
+	"tweets": 37952,
+	"created_at": "Sun Jun 19 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 18 10:51:10 +0000 2022",
+		"id": 1537986096725758000,
+		"id_str": "1538112077365125120",
+		"full_text": "Young Archibald prize 2022: winners announced for Australia’s cutest art prize – in pictures https://t.co/tHPQY2XA7o",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tHPQY2XA7o",
+					"expanded_url": "https://www.theguardian.com/artanddesign/gallery/2022/jun/18/young-archibald-prize-2022-winners-announced-for-australias-cutest-art-prize-in-pictures?CMP=twt_a-culture_b-gdnausculture_c-au_g-3",
+					"display_url": "theguardian.com/artanddesign/g…",
+					"indices": [
+						93,
+						116
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2206607906,
+			"id_str": "2206607906",
+			"name": "Guardian Aus Culture",
+			"screen_name": "GdnAusCulture",
+			"location": "Sydney, Australia",
+			"description": "Arts and ents news / views / reviews from @GuardianAus and wider @GuardianCulture world.",
+			"url": "http://t.co/Yoer0u3aEa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/Yoer0u3aEa",
+							"expanded_url": "http://www.theguardian.com/au/culture",
+							"display_url": "theguardian.com/au/culture",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2058,
+			"friends_count": 288,
+			"listed_count": 55,
+			"created_at": "Thu Nov 21 06:41:16 +0000 2013",
+			"favourites_count": 228,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3655,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065994193200390144/LpofX3Oj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065994193200390144/LpofX3Oj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2206607906/1542033712",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Young Archibald prize 2022: winners announced for Australia’s cutest art prize – in pictures https://t.co/tHPQY2XA7o"
+	},
+	{
 		"created_at": "Fri Jun 17 21:57:03 +0000 2022",
 		"id": 1537917262807855000,
 		"id_str": "1537917262807855104",
