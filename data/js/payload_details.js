855d27b017606cd48521fac49476b96dd9a84080
ellenforsyth <ellenforsyth@gmail.com>
2022-06-23 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37953,
-	"created_at": "Wed Jun 22 07:15:00 +0000 2022",
+	"tweets": 37954,
+	"created_at": "Thu Jun 23 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 23 00:32:51 +0000 2022",
+		"id": 1539768410401566700,
+		"id_str": "1539768410401566720",
+		"full_text": "@rgibli There are no guilty pleasures with reading - pleasures yes, guilt no 😊 Firekeeper's daughter by Angeline Boulley moves between slow &amp; fast paced (with some terrifying moments), Mary Robinette Kowal titles especially the Lady Astronaut series, Sleep well, my lady by Kwei Quartey",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			290
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rgibli",
+					"name": "Prof Rebecca Giblin",
+					"id": 256823402,
+					"id_str": "256823402",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1539762144123637800,
+		"in_reply_to_status_id_str": "1539762144123637760",
+		"in_reply_to_user_id": 256823402,
+		"in_reply_to_user_id_str": "256823402",
+		"in_reply_to_screen_name": "rgibli",
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2531,
+			"friends_count": 947,
+			"listed_count": 217,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 8959,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37631,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@rgibli There are no guilty pleasures with reading - pleasures yes, guilt no 😊 Firekeeper's daughter by Angeline Boulley moves between slow &amp; fast paced (with some terrifying moments), Mary Robinette Kowal titles especially the Lady Astronaut series, Sleep well, my lady by Kwei Quartey"
+	},
+	{
 		"created_at": "Tue Jun 21 10:29:49 +0000 2022",
 		"id": 1539193865655419000,
 		"id_str": "1539193865655418880",
