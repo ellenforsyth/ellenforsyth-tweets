bde07b53a435db096df484b25440935100ce99f2
ellenforsyth <ellenforsyth@gmail.com>
2022-07-08 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37970,
-	"created_at": "Thu Jul 07 07:15:00 +0000 2022",
+	"tweets": 37971,
+	"created_at": "Fri Jul 08 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 08 05:55:38 +0000 2022",
+		"id": 1544328095041298400,
+		"id_str": "1545285461832507394",
+		"full_text": "Our collection is archived at the British Library - it is called Voices of our National Health Service - some interviews are now catalogued online and available to listen to onsite - our @BL_OralHistory  archivist Hannah has written a blog for #NHS74\n\nhttps://t.co/w641HrMYR5\n🧵 8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NHS74",
+					"indices": [
+						244,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BL_OralHistory",
+					"name": "British Library Oral History",
+					"id": 2827421813,
+					"id_str": "2827421813",
+					"indices": [
+						187,
+						202
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/w641HrMYR5",
+					"expanded_url": "https://blogs.bl.uk/sound-and-vision/2022/07/recording-of-the-week-the-nhs-turns-74.html",
+					"display_url": "blogs.bl.uk/sound-and-visi…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1544328092847579100,
+		"in_reply_to_status_id_str": "1544328092847579139",
+		"in_reply_to_user_id": 899986581175640000,
+		"in_reply_to_user_id_str": "899986581175640065",
+		"in_reply_to_screen_name": "NHSat70",
+		"user": {
+			"id": 899986581175640000,
+			"id_str": "899986581175640065",
+			"name": "NHS Voices of Covid-19",
+			"screen_name": "NHSat70",
+			"location": "University of Manchester",
+			"description": "Creating a national collection of personal testimony on Covid-19 & the NHS/ archived @bl_oralhistory/ policy/ participatory research Funders #AHRC #UKRI #NLHF",
+			"url": "https://t.co/qHLWz67fKu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qHLWz67fKu",
+							"expanded_url": "http://nhs70.org.uk/",
+							"display_url": "nhs70.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4811,
+			"friends_count": 4368,
+			"listed_count": 0,
+			"created_at": "Tue Aug 22 13:28:16 +0000 2017",
+			"favourites_count": 4861,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3122,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1346067582533726208/hs5-BCWL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1346067582533726208/hs5-BCWL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/899986581175640065/1609764221",
+			"profile_link_color": "0AC8C8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our collection is archived at the British Library - it is called Voices of our National Health Service - some interviews are now catalogued online and available to listen to onsite - our @BL_OralHistory  archivist Hannah has written a blog for #NHS74\n\nhttps://t.co/w641HrMYR5\n🧵 8"
+	},
+	{
 		"created_at": "Thu Jul 07 03:27:10 +0000 2022",
 		"id": 1544692413540843500,
 		"id_str": "1544885711983476736",
