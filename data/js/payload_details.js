86df0d7d2d7070d64ece2ca5b1de44fe27f21cd3
ellenforsyth <ellenforsyth@gmail.com>
2022-08-05 17:15:01 +1000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37988,
-	"created_at": "Wed Aug 03 07:15:00 +0000 2022",
+	"tweets": 37990,
+	"created_at": "Fri Aug 05 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,10 +1,313 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 05 04:25:35 +0000 2022",
+		"id": 1555365226803077000,
+		"id_str": "1555409660358041600",
+		"full_text": "🚖 Beep beep - your ride is here!\n\nIn 1919, at just 20 years old, Alice Anderson founded Victoria's first all female motor garage.\n\nRead today's blog to learn about ‘Miss Anderson’s Motor Service’ 👇 \n\nhttps://t.co/GQonF18u5x\n\n 📷 Courtesy of University of Melbourne Archives https://t.co/EuV6BWLMO9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GQonF18u5x",
+					"expanded_url": "https://blogs.slv.vic.gov.au/our-stories/miss-andersons-motor-service/",
+					"display_url": "blogs.slv.vic.gov.au/our-stories/mi…",
+					"indices": [
+						200,
+						223
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1555365220834500600,
+					"id_str": "1555365220834500609",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FZXEbXvWYAEqrTz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZXEbXvWYAEqrTz.jpg",
+					"url": "https://t.co/EuV6BWLMO9",
+					"display_url": "pic.twitter.com/EuV6BWLMO9",
+					"expanded_url": "https://twitter.com/Library_Vic/status/1555365226803077120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1344,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 446,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 788,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1555365220834500600,
+					"id_str": "1555365220834500609",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FZXEbXvWYAEqrTz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZXEbXvWYAEqrTz.jpg",
+					"url": "https://t.co/EuV6BWLMO9",
+					"display_url": "pic.twitter.com/EuV6BWLMO9",
+					"expanded_url": "https://twitter.com/Library_Vic/status/1555365226803077120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1344,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 446,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 788,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 116586841,
+			"id_str": "116586841",
+			"name": "State Library Vic",
+			"screen_name": "Library_Vic",
+			"location": "Naarm / Melbourne, Australia ",
+			"description": "✨ Discover incredible collections, inspiring programs & trusted experts. 📖 Visit us 10am-6pm, daily.",
+			"url": "https://t.co/FesxywpJAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FesxywpJAp",
+							"expanded_url": "http://www.slv.vic.gov.au/",
+							"display_url": "slv.vic.gov.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36159,
+			"friends_count": 1039,
+			"listed_count": 830,
+			"created_at": "Tue Feb 23 00:40:03 +0000 2010",
+			"favourites_count": 13144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16703,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925594738991886336/7VFDlDQr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925594738991886336/7VFDlDQr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/116586841/1655965909",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F5EFA9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 35,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🚖 Beep beep - your ride is here!\n\nIn 1919, at just 20 years old, Alice Anderson founded Victoria's first all female motor garage.\n\nRead today's blog to learn about ‘Miss Anderson’s Motor Service’ 👇 \n\nhttps://t.co/GQonF18u5x\n\n 📷 Courtesy of University of Melbourne Archives https://t.co/EuV6BWLMO9"
+	},
+	{
+		"created_at": "Fri Aug 05 00:07:39 +0000 2022",
+		"id": 1555344748650832000,
+		"id_str": "1555344748650831872",
+		"full_text": "“Monday, Monday….” Turnbull and the Omicron surge https://t.co/uYO0pQBo3x via @nlnz good idea to have a blog post about this to inform the community about the challenges (and as a way of documenting current practice)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NLNZ",
+					"name": "National Library of New Zealand",
+					"id": 16248122,
+					"id_str": "16248122",
+					"indices": [
+						78,
+						83
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uYO0pQBo3x",
+					"expanded_url": "https://natlib.govt.nz/blog/posts/monday-monday-turnbull-and-the-omicron-surge",
+					"display_url": "natlib.govt.nz/blog/posts/mon…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2527,
+			"friends_count": 957,
+			"listed_count": 217,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 9351,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37677,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Monday, Monday….” Turnbull and the Omicron surge https://t.co/uYO0pQBo3x via @nlnz good idea to have a blog post about this to inform the community about the challenges (and as a way of documenting current practice)"
+	},
+	{
 		"created_at": "Tue Aug 02 19:41:57 +0000 2022",
 		"id": 1554553106352197600,
 		"id_str": "1554553106352197632",
-		"full_text": "Covid deaths in Australia: data reveals demographics hit hardest since pandemic began – interactive via ⁦@GuardianAus⁩  https://t.co/mDPesPvAey",
+		"full_text": "Covid deaths in Australia: data reveals demographics hit hardest since pandemic began – interactive via @GuardianAus  https://t.co/mDPesPvAey",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -116,7 +419,7 @@ Grailbird.data.tweets_2022_08 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Covid deaths in Australia: data reveals demographics hit hardest since pandemic began – interactive via ⁦@GuardianAus⁩  https://t.co/mDPesPvAey"
+		"text": "Covid deaths in Australia: data reveals demographics hit hardest since pandemic began – interactive via @GuardianAus  https://t.co/mDPesPvAey"
 	},
 	{
 		"created_at": "Mon Aug 01 00:31:08 +0000 2022",
