4973981dbf87a6d2f766baa7722a39272196e46c
ellenforsyth <ellenforsyth@gmail.com>
2022-08-06 17:15:01 +1000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37990,
-	"created_at": "Fri Aug 05 07:15:00 +0000 2022",
+	"tweets": 37992,
+	"created_at": "Sat Aug 06 07:15:00 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,428 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 05 23:30:58 +0000 2022",
+		"id": 1555697904685576200,
+		"id_str": "1555697904685576192",
+		"full_text": "You could substitute 'libraries' for archives... I found out about this via this article https://t.co/Cu4FTbey1y by Rachel Vagts (and good idea to go and read the article linked as well) https://t.co/kCDH8N9xFR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Cu4FTbey1y",
+					"expanded_url": "https://meridian.allenpress.com/american-archivist/article/85/1/5/483434/Radical-Welcome-in-the-Archives-What-Does-It",
+					"display_url": "meridian.allenpress.com/american-archi…",
+					"indices": [
+						89,
+						112
+					]
+				},
+				{
+					"url": "https://t.co/kCDH8N9xFR",
+					"expanded_url": "https://twitter.com/YCAL_JWJ/status/899348543957344256",
+					"display_url": "twitter.com/YCAL_JWJ/statu…",
+					"indices": [
+						187,
+						210
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2528,
+			"friends_count": 959,
+			"listed_count": 217,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 9383,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37680,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 899348543957344300,
+		"quoted_status_id_str": "899348543957344256",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kCDH8N9xFR",
+			"expanded": "https://twitter.com/YCAL_JWJ/status/899348543957344256",
+			"display": "twitter.com/YCAL_JWJ/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Aug 20 19:12:56 +0000 2017",
+			"id": 899348543957344300,
+			"id_str": "899348543957344256",
+			"full_text": "GUIDELINES: “Identifying and Dismantling White Supremacy in Archives” from Michelle Caswell @professorcaz https://t.co/QnXmKISKW0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				105
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "professorcaz",
+						"name": "Michelle Caswell",
+						"id": 946965716,
+						"id_str": "946965716",
+						"indices": [
+							92,
+							105
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 899348483261554700,
+						"id_str": "899348483261554688",
+						"indices": [
+							106,
+							129
+						],
+						"media_url": "http://pbs.twimg.com/media/DHsgrjoXYAAdAiQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHsgrjoXYAAdAiQ.jpg",
+						"url": "https://t.co/QnXmKISKW0",
+						"display_url": "pic.twitter.com/QnXmKISKW0",
+						"expanded_url": "https://twitter.com/YCAL_JWJ/status/899348543957344256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 606,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1070,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1826,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 899348483261554700,
+						"id_str": "899348483261554688",
+						"indices": [
+							106,
+							129
+						],
+						"media_url": "http://pbs.twimg.com/media/DHsgrjoXYAAdAiQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHsgrjoXYAAdAiQ.jpg",
+						"url": "https://t.co/QnXmKISKW0",
+						"display_url": "pic.twitter.com/QnXmKISKW0",
+						"expanded_url": "https://twitter.com/YCAL_JWJ/status/899348543957344256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 606,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1070,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1826,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 771832359356338200,
+				"id_str": "771832359356338176",
+				"name": "AmericanLit@Beinecke",
+				"screen_name": "YCAL_JWJ",
+				"location": "New Haven, CT",
+				"description": "News from the Beinecke Library's Yale Collection of American Literature and James Weldon Johnson Memorial Collection",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1856,
+				"friends_count": 252,
+				"listed_count": 24,
+				"created_at": "Fri Sep 02 22:09:09 +0000 2016",
+				"favourites_count": 5917,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 6295,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/772067781382733824/lq5hjmxi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/772067781382733824/lq5hjmxi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/771832359356338176/1473111534",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 11,
+		"favorite_count": 30,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You could substitute 'libraries' for archives... I found out about this via this article https://t.co/Cu4FTbey1y by Rachel Vagts (and good idea to go and read the article linked as well) https://t.co/kCDH8N9xFR"
+	},
+	{
+		"created_at": "Fri Aug 05 22:45:06 +0000 2022",
+		"id": 1555686359461171200,
+		"id_str": "1555686359461171200",
+		"full_text": "Acoustic Life of Farm Sheds &amp; Landscapes of Learning https://t.co/m7OSANd1ub via @newcardigan this is a great idea - sheds, local studies and music (and it has run in previous years, and there was a boatshed version in Sydney earlier this year https://t.co/gFPbT4fNwH)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "newcardigan",
+					"name": "newCardigan",
+					"id": 1106084318,
+					"id_str": "1106084318",
+					"indices": [
+						85,
+						97
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/m7OSANd1ub",
+					"expanded_url": "https://www.agricultured.com.au/event/landscapes-of-learning-2/",
+					"display_url": "agricultured.com.au/event/landscap…",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/gFPbT4fNwH",
+					"expanded_url": "https://2022.sydneyfestival.org.au/events/acoustic-life-of-boatsheds",
+					"display_url": "2022.sydneyfestival.org.au/events/acousti…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2528,
+			"friends_count": 959,
+			"listed_count": 217,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 9383,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37680,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Acoustic Life of Farm Sheds &amp; Landscapes of Learning https://t.co/m7OSANd1ub via @newcardigan this is a great idea - sheds, local studies and music (and it has run in previous years, and there was a boatshed version in Sydney earlier this year https://t.co/gFPbT4fNwH)"
+	},
+	{
 		"created_at": "Fri Aug 05 04:25:35 +0000 2022",
 		"id": 1555365226803077000,
 		"id_str": "1555409660358041600",
