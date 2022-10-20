f086459c3b4dc2c18228c436e418cb06b45d95c2
ellenforsyth <ellenforsyth@gmail.com>
2022-10-20 18:15:02 +1100

    Added 2 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,448 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 19 22:41:42 +0000 2022",
+		"id": 1582797977516904400,
+		"id_str": "1582864595651526656",
+		"full_text": "Chemical attraction: some people really are ‘mosquito magnets’, study finds https://t.co/CJz6HADktX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CJz6HADktX",
+					"expanded_url": "https://www.theguardian.com/science/2022/oct/19/mosquito-magnets-human-smell-chemicals-skin-study?utm_term=Autofeed&CMP=twt_gu&utm_medium&utm_source=Twitter#Echobox=1666186317",
+					"display_url": "theguardian.com/science/2022/o…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 87818409,
+			"id_str": "87818409",
+			"name": "The Guardian",
+			"screen_name": "guardian",
+			"location": "London",
+			"description": "The need for independent journalism has never been greater. Become a Guardian supporter: https://t.co/gWyuUVlObq",
+			"url": "https://t.co/c53pnmnuIT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c53pnmnuIT",
+							"expanded_url": "https://www.theguardian.com",
+							"display_url": "theguardian.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/gWyuUVlObq",
+							"expanded_url": "https://support.theguardian.com",
+							"display_url": "support.theguardian.com",
+							"indices": [
+								89,
+								112
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10801347,
+			"friends_count": 1061,
+			"listed_count": 70715,
+			"created_at": "Thu Nov 05 23:49:19 +0000 2009",
+			"favourites_count": 139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 780755,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1175141826870861825/K2qKoGla_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1175141826870861825/K2qKoGla_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/87818409/1620214786",
+			"profile_link_color": "005789",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "CAE3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 35,
+		"favorite_count": 122,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Chemical attraction: some people really are ‘mosquito magnets’, study finds https://t.co/CJz6HADktX"
+	},
+	{
+		"created_at": "Wed Oct 19 21:24:23 +0000 2022",
+		"id": 1582845136643686400,
+		"id_str": "1582845136643686400",
+		"full_text": "Interesting data - and a reminder of the importance of #readadv for promoting all of your collection, not just the titles in the news via @artefactors https://t.co/274A8BwDiY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "readadv",
+					"indices": [
+						55,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "artefactors",
+					"name": "artefacto",
+					"id": 880391678,
+					"id_str": "880391678",
+					"indices": [
+						138,
+						150
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/274A8BwDiY",
+					"expanded_url": "https://twitter.com/BookNet_Canada/status/1578420143083978754",
+					"display_url": "twitter.com/BookNet_Canada…",
+					"indices": [
+						151,
+						174
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, content creation, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country.",
+			"url": "https://t.co/KIxKPRNDhv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPRNDhv",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2330,
+			"friends_count": 980,
+			"listed_count": 216,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 9918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37778,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1578420143083978800,
+		"quoted_status_id_str": "1578420143083978754",
+		"quoted_status_permalink": {
+			"url": "https://t.co/274A8BwDiY",
+			"expanded": "https://twitter.com/BookNet_Canada/status/1578420143083978754",
+			"display": "twitter.com/BookNet_Canada…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Oct 07 16:21:02 +0000 2022",
+			"id": 1578420143083978800,
+			"id_str": "1578420143083978754",
+			"full_text": "Find out the real impact of #BookTok on the Canadian library circulation of backlist titles in this blog post: https://t.co/2Gm3M1X3lA https://t.co/JvMp8r0U6b",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				134
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "BookTok",
+						"indices": [
+							28,
+							36
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/2Gm3M1X3lA",
+						"expanded_url": "https://buff.ly/3CGzHIT",
+						"display_url": "buff.ly/3CGzHIT",
+						"indices": [
+							111,
+							134
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1578420140290576400,
+						"id_str": "1578420140290576389",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/media/FeeswThXkAUj8Zm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FeeswThXkAUj8Zm.jpg",
+						"url": "https://t.co/JvMp8r0U6b",
+						"display_url": "pic.twitter.com/JvMp8r0U6b",
+						"expanded_url": "https://twitter.com/BookNet_Canada/status/1578420143083978754/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1000,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 272,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1000,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1578420140290576400,
+						"id_str": "1578420140290576389",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/media/FeeswThXkAUj8Zm.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FeeswThXkAUj8Zm.jpg",
+						"url": "https://t.co/JvMp8r0U6b",
+						"display_url": "pic.twitter.com/JvMp8r0U6b",
+						"expanded_url": "https://twitter.com/BookNet_Canada/status/1578420143083978754/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1000,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 272,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1000,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14615687,
+				"id_str": "14615687",
+				"name": "BookNet Canada",
+				"screen_name": "BookNet_Canada",
+				"location": "Toronto",
+				"description": "BookNet Canada builds technology solutions that make it easier for Canadians to promote and sell books. Follow us for book industry news and BNC updates.",
+				"url": "https://t.co/p2DjzwjIPM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/p2DjzwjIPM",
+								"expanded_url": "http://www.booknetcanada.ca",
+								"display_url": "booknetcanada.ca",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8794,
+				"friends_count": 877,
+				"listed_count": 431,
+				"created_at": "Thu May 01 15:52:55 +0000 2008",
+				"favourites_count": 5060,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 14954,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "789997",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/925714874193162240/bISgl906_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/925714874193162240/bISgl906_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14615687/1574114106",
+				"profile_link_color": "4D8FAA",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F5F5F5",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interesting data - and a reminder of the importance of #readadv for promoting all of your collection, not just the titles in the news via @artefactors https://t.co/274A8BwDiY"
+	},
+	{
 		"created_at": "Mon Oct 17 09:19:25 +0000 2022",
 		"id": 1581937918481875000,
 		"id_str": "1581937918481874945",
