09aa073638d73a784a24427b0191f066b5ffba07
ellenforsyth <ellenforsyth@gmail.com>
2022-11-17 18:15:07 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38119,
-	"created_at": "Tue Nov 15 07:15:00 +0000 2022",
+	"tweets": 38120,
+	"created_at": "Thu Nov 17 07:15:06 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 16 18:13:25 +0000 2022",
+		"id": 1592747366406582300,
+		"id_str": "1592943941787463686",
+		"full_text": "Want to learn more about prisoner information needs and libraries in prisons? This free seminar will be of interest: https://t.co/CUAzCRWmFz I'm looking forward to presenting, with a prison librarian and a prison library user. #prisonlibraries",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			243
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "prisonlibraries",
+					"indices": [
+						227,
+						243
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CUAzCRWmFz",
+					"expanded_url": "https://tinyurl.com/ycktba3t",
+					"display_url": "tinyurl.com/ycktba3t",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3060607604,
+			"id_str": "3060607604",
+			"name": "Dr Jane Garner",
+			"screen_name": "DrJaneGarner",
+			"location": "Australia",
+			"description": "Information Studies academic. Charles Sturt University. Associate Editor - The Library Quarterly. ARC DECRA Fellow 2022.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 868,
+			"friends_count": 1068,
+			"listed_count": 19,
+			"created_at": "Wed Mar 04 00:15:56 +0000 2015",
+			"favourites_count": 3284,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1968,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1050235709750095873/twNc8zcB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1050235709750095873/twNc8zcB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3060607604/1539230466",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want to learn more about prisoner information needs and libraries in prisons? This free seminar will be of interest: https://t.co/CUAzCRWmFz I'm looking forward to presenting, with a prison librarian and a prison library user. #prisonlibraries"
+	},
+	{
 		"created_at": "Mon Nov 14 07:57:24 +0000 2022",
 		"id": 1592064137357844500,
 		"id_str": "1592064137357844480",
