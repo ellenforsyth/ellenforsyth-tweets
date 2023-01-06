3ddb14d73ea304db46dfd91be92434e0864437b9
ellenforsyth <ellenforsyth@gmail.com>
2023-01-06 18:15:02 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38144,
-	"created_at": "Fri Dec 23 07:15:00 +0000 2022",
+	"tweets": 38145,
+	"created_at": "Fri Jan 06 07:15:01 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,142 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Thu Jan 05 19:12:35 +0000 2023",
+		"id": 1611077673513799700,
+		"id_str": "1611078221516374016",
+		"full_text": "National Library of Australia’s free digital archives may be forced to close without funding #Trove #saveTrove #fundTrove  https://t.co/XDmu67HpqU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Trove",
+					"indices": [
+						93,
+						99
+					]
+				},
+				{
+					"text": "saveTrove",
+					"indices": [
+						100,
+						110
+					]
+				},
+				{
+					"text": "fundTrove",
+					"indices": [
+						111,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XDmu67HpqU",
+					"expanded_url": "https://www.theguardian.com/culture/2023/jan/06/national-library-of-australias-free-digital-archives-may-be-forced-to-close-without-funding?CMP=Share_iOSApp_Other",
+					"display_url": "theguardian.com/culture/2023/j…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 386814685,
+			"id_str": "386814685",
+			"name": "Dr Mike Jones",
+			"screen_name": "MikeJonesPhD",
+			"location": "Ngunnawal and Ngambri Country",
+			"description": "Archives, history, collections, museums, digital humanities, art. Books, music, film. Queer. Still goth at heart. He/him. https://t.co/TCYYvYFihb",
+			"url": "https://t.co/4yrCqkIn6O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4yrCqkIn6O",
+							"expanded_url": "http://www.mikejonesonline.com/",
+							"display_url": "mikejonesonline.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/TCYYvYFihb",
+							"expanded_url": "http://mastodon.social/@mikejones",
+							"display_url": "mastodon.social/@mikejones",
+							"indices": [
+								122,
+								145
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4851,
+			"friends_count": 1989,
+			"listed_count": 273,
+			"created_at": "Fri Oct 07 23:33:41 +0000 2011",
+			"favourites_count": 67484,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59760,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1577505685/ProfilePic_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1577505685/ProfilePic_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/386814685/1398208719",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 28,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "National Library of Australia’s free digital archives may be forced to close without funding #Trove #saveTrove #fundTrove  https://t.co/XDmu67HpqU"
+	}
+]
