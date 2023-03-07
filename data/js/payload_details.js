abee6ce32357fab756badad960c1dec1bc4959d6
ellenforsyth <ellenforsyth@gmail.com>
2023-03-07 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38163,
-	"created_at": "Sun Mar 05 07:15:00 +0000 2023",
+	"tweets": 38164,
+	"created_at": "Tue Mar 07 07:15:00 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,10 +1,117 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Mon Mar 06 18:25:13 +0000 2023",
+		"id": 1632809573278179300,
+		"id_str": "1632809573278179328",
+		"full_text": "From lollies and baked beans to internet cafes and selfie sticks: The long history of the Australian corner shop - ABC News - this is a reminder of the importance of documenting ‘ordinary’ features of your community for your local studies collections. https://t.co/QFoRnmm1PN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QFoRnmm1PN",
+					"expanded_url": "https://www.abc.net.au/news/2023-03-07/the-enduring-legacy-of-the-humble-australian-corner-store/102057742",
+					"display_url": "abc.net.au/news/2023-03-0…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16970274,
+			"id_str": "16970274",
+			"name": "ellen forsyth (she/her)",
+			"screen_name": "ellenforsyth",
+			"location": "NSW, Australia",
+			"description": "😷 libraries, reading, local studies, games, climate and a few other things. The opinions are my own. On Gadigal and Wangal country. @ellenforsyth@ausglam.space",
+			"url": "https://t.co/KIxKPS5MvD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KIxKPS5MvD",
+							"expanded_url": "http://readplayparticipate.blogspot.com/",
+							"display_url": "readplayparticipate.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2268,
+			"friends_count": 967,
+			"listed_count": 210,
+			"created_at": "Sat Oct 25 19:15:16 +0000 2008",
+			"favourites_count": 10126,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 37790,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "001329",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1088585514247061504/5UI3uYwa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16970274/1645174236",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "000B17",
+			"profile_text_color": "004358",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "From lollies and baked beans to internet cafes and selfie sticks: The long history of the Australian corner shop - ABC News - this is a reminder of the importance of documenting ‘ordinary’ features of your community for your local studies collections. https://t.co/QFoRnmm1PN"
+	},
+	{
 		"created_at": "Sat Mar 04 08:33:50 +0000 2023",
 		"id": 1631935972714901500,
 		"id_str": "1631935972714901506",
-		"full_text": "The joy of mediocrity: we need hobbies, even if we’re bad at them, to free us from perfection via ⁦@GuardianAus⁩  https://t.co/fRGJV6pwqk",
+		"full_text": "The joy of mediocrity: we need hobbies, even if we’re bad at them, to free us from perfection via @GuardianAus  https://t.co/fRGJV6pwqk",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -116,7 +223,7 @@ Grailbird.data.tweets_2023_03 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "The joy of mediocrity: we need hobbies, even if we’re bad at them, to free us from perfection via ⁦@GuardianAus⁩  https://t.co/fRGJV6pwqk"
+		"text": "The joy of mediocrity: we need hobbies, even if we’re bad at them, to free us from perfection via @GuardianAus  https://t.co/fRGJV6pwqk"
 	},
 	{
 		"created_at": "Wed Mar 01 21:47:45 +0000 2023",
