226f4b5d01f3ff18e479f4f57c953322cee49ed3
ellenforsyth <ellenforsyth@gmail.com>
2023-03-19 18:15:01 +1100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38165,
-	"created_at": "Sun Mar 12 07:15:00 +0000 2023",
+	"tweets": 38166,
+	"created_at": "Sun Mar 19 07:15:01 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 18 09:44:01 +0000 2023",
+		"id": 1636834851809525800,
+		"id_str": "1637027064653168640",
+		"full_text": "🕷️ NEW SPECIES 🕷️ Our scientists have described a rare giant species of trapdoor spider only found in Central QLD. Euoplos dignitas is likely endangered, having lost much of its habitat due to land clearing. Learn more 👉 https://t.co/P73BcPIAK5\nSupported by @BHP #ProjectDIGQM https://t.co/30k0VDUcbi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ProjectDIGQM",
+					"indices": [
+						263,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bhp",
+					"name": "BHP",
+					"id": 2220388494,
+					"id_str": "2220388494",
+					"indices": [
+						258,
+						262
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/P73BcPIAK5",
+					"expanded_url": "http://bit.ly/3kA1Aw3",
+					"display_url": "bit.ly/3kA1Aw3",
+					"indices": [
+						221,
+						244
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1636834809858146300,
+					"id_str": "1636834809858146308",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Frc0llWWwAQJGVx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Frc0llWWwAQJGVx.jpg",
+					"url": "https://t.co/30k0VDUcbi",
+					"display_url": "pic.twitter.com/30k0VDUcbi",
+					"expanded_url": "https://twitter.com/qldmuseum/status/1636834851809525761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1636834809858146300,
+					"id_str": "1636834809858146308",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Frc0llWWwAQJGVx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Frc0llWWwAQJGVx.jpg",
+					"url": "https://t.co/30k0VDUcbi",
+					"display_url": "pic.twitter.com/30k0VDUcbi",
+					"expanded_url": "https://twitter.com/qldmuseum/status/1636834851809525761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1636834830879998000,
+					"id_str": "1636834830879997956",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Frc0mzqWwAQfsHj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Frc0mzqWwAQfsHj.jpg",
+					"url": "https://t.co/30k0VDUcbi",
+					"display_url": "pic.twitter.com/30k0VDUcbi",
+					"expanded_url": "https://twitter.com/qldmuseum/status/1636834851809525761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37788263,
+			"id_str": "37788263",
+			"name": "Queensland Museum",
+			"screen_name": "qldmuseum",
+			"location": "Turrbal and Jagera Country / Brisbane",
+			"description": "Share your snaps with us #myqldmuseum",
+			"url": "https://t.co/w7sct4BsbO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w7sct4BsbO",
+							"expanded_url": "https://www.qm.qld.gov.au/",
+							"display_url": "qm.qld.gov.au",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12450,
+			"friends_count": 1161,
+			"listed_count": 339,
+			"created_at": "Mon May 04 23:10:44 +0000 2009",
+			"favourites_count": 15471,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 13509,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1268655361944240128/PHWEPSoU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1268655361944240128/PHWEPSoU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37788263/1643327857",
+			"profile_link_color": "8F9B8E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "333333",
+			"profile_text_color": "FFFFFF",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 26,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🕷️ NEW SPECIES 🕷️ Our scientists have described a rare giant species of trapdoor spider only found in Central QLD. Euoplos dignitas is likely endangered, having lost much of its habitat due to land clearing. Learn more 👉 https://t.co/P73BcPIAK5\nSupported by @BHP #ProjectDIGQM https://t.co/30k0VDUcbi"
+	},
+	{
 		"created_at": "Sun Mar 12 06:05:58 +0000 2023",
 		"id": 1628082293007954000,
 		"id_str": "1634797865422979075",
