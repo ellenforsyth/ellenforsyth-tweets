e0d9d6b820977e7d0b7d7903059019a0cace0fee
ellenforsyth <ellenforsyth@gmail.com>
2023-04-27 17:15:01 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38174,
-	"created_at": "Mon Apr 24 07:15:05 +0000 2023",
+	"tweets": 38175,
+	"created_at": "Thu Apr 27 07:15:00 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,10 +1,104 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Wed Apr 26 19:24:58 +0000 2023",
+		"id": 1651304124145881000,
+		"id_str": "1651306393477971990",
+		"full_text": "\"The language that we use in the catalog reflects our values and beliefs in the same way as a gallery label. Instead of continuing to use outdated and offensive subject headings, we have chosen more appropriate and inclusive language . . .\" https://t.co/IaY4wh5QOA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IaY4wh5QOA",
+					"expanded_url": "https://bit.ly/3mZV42R",
+					"display_url": "bit.ly/3mZV42R",
+					"indices": [
+						241,
+						264
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1651304122698825700,
+		"in_reply_to_status_id_str": "1651304122698825729",
+		"in_reply_to_user_id": 1279421198,
+		"in_reply_to_user_id_str": "1279421198",
+		"in_reply_to_screen_name": "mimosaishere",
+		"user": {
+			"id": 1279421198,
+			"id_str": "1279421198",
+			"name": "impressionable, considered, cared for",
+			"screen_name": "mimosaishere",
+			"location": "",
+			"description": "library worker | museum nerd | @ala_spectrum scholar | @iSchoolUI MS/LIS graduate | she-her | never the main character | views my own",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1307,
+			"friends_count": 467,
+			"listed_count": 36,
+			"created_at": "Tue Mar 19 03:26:11 +0000 2013",
+			"favourites_count": 52084,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23398,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "2E252B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220052430383144962/B_EQoz77_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220052430383144962/B_EQoz77_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1279421198/1398222763",
+			"profile_link_color": "FC744B",
+			"profile_sidebar_border_color": "D2D3AF",
+			"profile_sidebar_fill_color": "F1F28D",
+			"profile_text_color": "FAC956",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"The language that we use in the catalog reflects our values and beliefs in the same way as a gallery label. Instead of continuing to use outdated and offensive subject headings, we have chosen more appropriate and inclusive language . . .\" https://t.co/IaY4wh5QOA"
+	},
+	{
 		"created_at": "Sun Apr 23 19:44:30 +0000 2023",
 		"id": 1650224143433085000,
 		"id_str": "1650224143433084928",
-		"full_text": "'We are invisible': Veteran Del says women still asked whose medals they are wearing via ⁦@abcnews⁩  https://t.co/fWzR3i4F0Y",
+		"full_text": "'We are invisible': Veteran Del says women still asked whose medals they are wearing via @abcnews  https://t.co/fWzR3i4F0Y",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -116,7 +210,7 @@ Grailbird.data.tweets_2023_04 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "'We are invisible': Veteran Del says women still asked whose medals they are wearing via ⁦@abcnews⁩  https://t.co/fWzR3i4F0Y"
+		"text": "'We are invisible': Veteran Del says women still asked whose medals they are wearing via @abcnews  https://t.co/fWzR3i4F0Y"
 	},
 	{
 		"created_at": "Sat Apr 15 01:39:02 +0000 2023",
